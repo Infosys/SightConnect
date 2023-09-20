249769e6b69260c73d683a3edc4dfe19ef44de57
Avendra Singh <avendrasingh.gaur@infosys.com>
2023-09-20 16:07:37 +0530

    updates

--- lib/roles/patient/patient_home/presentation/providers/patient_home_provider.dart
+++ lib/roles/patient/patient_home/presentation/providers/patient_home_provider.dart
@@ -2,8 +2,6 @@ import 'package:eye_care_for_all/roles/patient/patient_home/presentation/pages/p
 import 'package:eye_care_for_all/roles/patient/patient_notification/presentation/pages/patient_notification_page.dart';
 import 'package:eye_care_for_all/roles/patient/patient_profile/presentation/pages/patient_profile_page.dart';
 import 'package:eye_care_for_all/roles/patient/patient_services/presentation/pages/patient_services_page.dart';
-import 'package:eye_care_for_all/roles/patient/patient_triage_eye_scan/presentation/pages/patient_triage_eye_scan_intro_page.dart';
-import 'package:eye_care_for_all/roles/patient/patient_triage_steps/presentation/pages/patient_triage_steps_page.dart';
 import 'package:eye_care_for_all/roles/patient/patient_tumbling_test/presentation/pages/tumbling_test_initiate_page.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -21,9 +19,9 @@ class PatientHomeProvider extends ChangeNotifier {
   }
 
   List<Widget> pages = [
-    const PatientTriageEyeScanIntroPage(),
+    const PatientHomePage(),
     const PatientServicesPage(),
-    const PatientTriageStepsPage(),
+    const TumblingTestInitiatePage(),
     const PatientNotificationPage(),
     const PatientProfilePage()
   ];
