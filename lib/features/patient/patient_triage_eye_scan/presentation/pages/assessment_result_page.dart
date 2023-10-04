f96bf0c349c13f61a80b9123cd3ce39e504a1721
Sarthak Paranjpe <sarthak.paranjpe@infosys.com>
2023-10-04 09:53:34 +0530

    result page changes

--- lib/features/patient/patient_triage_eye_scan/presentation/pages/assessment_result_page.dart
+++ lib/features/patient/patient_triage_eye_scan/presentation/pages/assessment_result_page.dart
@@ -21,7 +21,7 @@ class AssessmentResultPage extends ConsumerWidget {
 //state - Completed, Complete test, Not Applicable
 //issueLevel - Normal, Minor, Major
     var resultData = ref.watch(resultStateProvider);
-    resultData.setResultState(0, 'Completed', 'Normal');
+    resultData.setResultState(1, 'Complete test', 'Normal');
 
     // List resultState = [
     //   {
@@ -223,83 +223,65 @@ class AssessmentResultPage extends ConsumerWidget {
                   ],
                 ),
               ),
-              SizedBox(
-                height: AppSize.height(context) * 0.14,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Flexible(
-                      child: ListView.builder(
-                        shrinkWrap: true,
-                        physics: const NeverScrollableScrollPhysics(),
-                        scrollDirection: Axis.horizontal,
-                        itemCount: resultData.resultState.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          resultData.setColors(index);
-
-                          return Expanded(
-                            child: Container(
-                              padding: EdgeInsets.only(
-                                  left: AppSize.width(context) * 0.03,
-                                  right: AppSize.width(context) * 0.048,
-                                  top: AppSize.height(context) * 0.01),
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(8),
-                                color: resultData.backColor,
-                              ),
-                              margin: index == 2
-                                  ? const EdgeInsets.only(
-                                      right: 0,
-                                    )
-                                  : EdgeInsets.only(
-                                      right: AppSize.width(context) * 0.04),
-                              child: Column(
-                                  mainAxisSize: MainAxisSize.min,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Container(
-                                      width: 20,
-                                      height: 20,
-                                      decoration: BoxDecoration(
-                                        shape: BoxShape.circle,
-                                        color: resultData.checkColor,
-                                      ),
-                                      child: Center(
-                                        child: Icon(
-                                          resultData.icon,
-                                          color: Colors.white,
-                                          size: 15,
-                                        ),
-                                      ),
-                                    ),
-                                    const SizedBox(
-                                      height: 8,
-                                    ),
-                                    Text(
-                                      resultData.resultState[index]['type']!,
-                                      style: applyRobotoFont(
-                                          fontSize: 14,
-                                          fontWeight: FontWeight.w600),
-                                    ),
-                                    const SizedBox(
-                                      height: 8,
-                                    ),
-                                    Text(
-                                      resultData.resultState[index]['state']!,
-                                      style: applyRobotoFont(
-                                          fontSize: 12,
-                                          fontWeight: FontWeight.w400),
-                                    )
-                                  ]),
-                            ),
-                          );
-                        },
+              GridView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  padding: EdgeInsets.zero,
+                  itemCount: resultData.resultState.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    childAspectRatio: 1,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                  ),
+                  itemBuilder: (context, index) {
+                    resultData.setColors(index);
+                    return Container(
+                      padding: EdgeInsets.only(
+                          left: AppSize.width(context) * 0.03,
+                          top: AppSize.height(context) * 0.01),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        color: resultData.backColor,
                       ),
-                    ),
-                  ],
-                ),
-              ),
+                      child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              width: 20,
+                              height: 20,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: resultData.checkColor,
+                              ),
+                              child: Center(
+                                child: Icon(
+                                  resultData.icon,
+                                  color: Colors.white,
+                                  size: 15,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 8,
+                            ),
+                            Text(
+                              resultData.resultState[index]['type']!,
+                              style: applyRobotoFont(
+                                  fontSize: 14, fontWeight: FontWeight.w600),
+                            ),
+                            const SizedBox(
+                              height: 8,
+                            ),
+                            Text(
+                              resultData.resultState[index]['state']!,
+                              style: applyRobotoFont(
+                                  fontSize: 12, fontWeight: FontWeight.w400),
+                            )
+                          ]),
+                    );
+                  }),
               const SizedBox(
                 height: AppSize.kmheight,
               ),
