66686cad7fff632ef85cbc38c63f2ebcdc8eb613
Abhishek Singh(abhishek.singh169) <abhishek.singh169@infosys.com>
2023-10-05 17:08:40 +0530

    fixed bugs

--- lib/features/patient/patient_assessments_and_tests/presentation/pages/patient_assessments_and_tests_page.dart
+++ lib/features/patient/patient_assessments_and_tests/presentation/pages/patient_assessments_and_tests_page.dart
@@ -15,117 +15,114 @@ class AssessmentsAndTestsPage extends HookWidget {
     var selectedOption = useState<String>(people[0]['name']);
     var isDropDownOpen = useState<bool>(false);
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Assessments and Tests"),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(AppSize.klpadding),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Here is a quick view of the assessments done so far.",
-                style: applyRobotoFont(
-                  color: Colors.black,
-                  fontWeight: FontWeight.w400,
-                  fontSize: 14,
-                ),
+      appBar: AppBar(
+        title: const Text("Assessments and Tests"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(AppSize.klpadding),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Here is a quick view of the assessments done so far.",
+              style: applyRobotoFont(
+                color: Colors.black,
+                fontWeight: FontWeight.w400,
+                fontSize: 14,
               ),
-              const SizedBox(
-                height: AppSize.kmheight,
-              ),
-              Row(
-                children: [
-                  InkWell(
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 12,
-                        vertical: 5,
-                      ),
-                      decoration: BoxDecoration(
-                        color: AppColor.primary,
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      child: Text(
-                        "All",
-                        style: applyRobotoFont(
-                          color: AppColor.white,
-                          fontWeight: FontWeight.w400,
-                          fontSize: 14,
-                        ),
-                      ),
+            ),
+            const SizedBox(height: AppSize.kmheight),
+            Row(
+              children: [
+                InkWell(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 12,
+                      vertical: 5,
                     ),
-                  ),
-                  const SizedBox(
-                    width: AppSize.kswidth,
-                  ),
-                  Container(
-                    height: 30,
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(40),
-                      border: Border.all(
-                          color: isDropDownOpen.value
-                              ? AppColor.primary
-                              : AppColor.black),
-                      color: isDropDownOpen.value
-                          ? AppColor.primary
-                          : AppColor.white,
+                      color: AppColor.primary,
+                      borderRadius: BorderRadius.circular(30),
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 8.0, vertical: 0),
-                      child: DropdownButtonHideUnderline(
-                        child: DropdownButton<String>(
-                          value: selectedOption.value,
-                          onChanged: (newValue) {
-                            selectedOption.value = newValue!;
-                            isDropDownOpen.value = true;
-                          },
-                          dropdownColor: AppColor.primary,
-                          icon: Icon(
-                            CupertinoIcons.chevron_down,
-                            color: isDropDownOpen.value
-                                ? AppColor.white
-                                : AppColor.black,
-                          ),
-                          iconSize: 18,
-                          elevation: 0,
-                          padding: EdgeInsets.zero,
-                          items: people.map((person) {
-                            return DropdownMenuItem<String>(
-                              value: person['name'],
-                              child: Row(
-                                children: [
-                                  Image.asset(
-                                    person['image'],
-                                    width: 25,
-                                    height: 25,
-                                  ),
-                                  const SizedBox(width: 10),
-                                  Text(
-                                    person['name'],
-                                    style: applyRobotoFont(
-                                      fontSize: 14,
-                                      color: isDropDownOpen.value
-                                          ? AppColor.white
-                                          : AppColor.black,
-                                      fontWeight: FontWeight.w400,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            );
-                          }).toList(),
-                        ),
+                    child: Text(
+                      "All",
+                      style: applyRobotoFont(
+                        color: AppColor.white,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 14,
                       ),
                     ),
                   ),
-                ],
-              ),
-              const Flexible(child: AssessmentCards()),
-            ],
-          ),
-        ));
+                ),
+                const SizedBox(
+                  width: AppSize.kswidth,
+                ),
+                Container(
+                  height: 30,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(40),
+                    border: Border.all(
+                      color: AppColor.black.withOpacity(0.6),
+                      width: 1,
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 8.0,
+                      vertical: 0,
+                    ),
+                    child: DropdownButtonHideUnderline(
+                      child: DropdownButton<String>(
+                        value: selectedOption.value,
+                        onChanged: (newValue) {
+                          selectedOption.value = newValue!;
+                          isDropDownOpen.value = true;
+                        },
+                        icon: Icon(
+                          CupertinoIcons.chevron_down,
+                          color: isDropDownOpen.value
+                              ? AppColor.white
+                              : AppColor.black,
+                        ),
+                        iconSize: 18,
+                        elevation: 0,
+                        dropdownColor: AppColor.white,
+                        items: people.map((person) {
+                          return DropdownMenuItem<String>(
+                            enabled: true,
+                            value: person['name'],
+                            child: Row(
+                              children: [
+                                Image.asset(
+                                  person['image'],
+                                  width: 25,
+                                  height: 25,
+                                ),
+                                const SizedBox(width: 10),
+                                Text(
+                                  person['name'],
+                                  style: applyRobotoFont(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        }).toList(),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: AppSize.kmheight),
+            const Expanded(
+              child: AssessmentCards(),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/features/patient/patient_assessments_and_tests/presentation/widgets/assements_cards.dart
+++ lib/features/patient/patient_assessments_and_tests/presentation/widgets/assements_cards.dart
@@ -2,6 +2,7 @@ import 'package:eye_care_for_all/core/constants/app_color.dart';
 import 'package:eye_care_for_all/core/constants/app_size.dart';
 import 'package:eye_care_for_all/features/patient/patient_assessments_and_tests/data/fake_data_source.dart';
 import 'package:eye_care_for_all/features/patient/patient_assessments_and_tests/presentation/pages/patient_assessment_report_page.dart';
+import 'package:eye_care_for_all/features/patient/patient_assessments_and_tests/presentation/pages/patient_test_timeline_page.dart';
 import 'package:eye_care_for_all/shared/theme/text_theme.dart';
 import 'package:flutter/material.dart';
 
@@ -10,201 +11,189 @@ class AssessmentCards extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(0),
-      child: SizedBox(
-        child: Expanded(
-          child: ListView.builder(
-            itemCount: data.length,
-            itemBuilder: (BuildContext context, int index) {
-              var currentData = data[index];
-              return SizedBox(
-                height: AppSize.height(context) * 0.23,
-                child: Card(
-                  elevation: 2,
-                  clipBehavior: Clip.hardEdge,
-                  child: Container(
-                    padding: const EdgeInsets.all(14),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.0),
+    return ListView.builder(
+      itemCount: data.length,
+      itemBuilder: (BuildContext context, int index) {
+        var currentData = data[index];
+        return Card(
+          elevation: 2,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+          child: Container(
+            padding: const EdgeInsets.all(14),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(4),
+                      decoration: BoxDecoration(
+                        color: currentData['checkupType'] == "Routine Checkup"
+                            ? AppColor.green
+                            : AppColor.orange,
+                      ),
+                      child: Text(
+                        currentData['checkupType'],
+                        style: applyRobotoFont(
+                          fontSize: 11,
+                          fontWeight: FontWeight.w500,
+                          color: AppColor.white,
+                        ),
+                      ),
                     ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
+                    const Spacer(),
+                    Container(
+                      padding: const EdgeInsets.all(4),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: currentData['reminderMessage'] == "Post OPS Care"
+                            ? AppColor.green.withOpacity(0.6)
+                            : currentData['reminderMessage'] ==
+                                    "3rd Reminder Sent"
+                                ? AppColor.red.withOpacity(0.4)
+                                : AppColor.green.withOpacity(0.4),
+                      ),
+                      child: Text(
+                        currentData['reminderMessage'],
+                        style: applyRobotoFont(
+                          fontSize: 11,
+                          fontWeight: FontWeight.w500,
+                          color: AppColor.black,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  children: [
+                    Image.asset(
+                      currentData['image'],
+                      height: 25,
+                      width: 25,
+                    ),
+                    const SizedBox(width: 8),
+                    Text(
+                      currentData['name'],
+                      style: applyRobotoFont(
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.min,
                       children: [
-                        Row(
-                          children: [
-                            Container(
-                                padding: EdgeInsets.symmetric(
-                                  horizontal: AppSize.width(context) * 0.01,
-                                ),
-                                decoration: BoxDecoration(
-                                  color: currentData['checkupType'] ==
-                                          "Routine Checkup"
-                                      ? AppColor.green
-                                      : AppColor.orange,
-                                ),
-                                child: Text(
-                                  currentData['checkupType'],
-                                  style: applyRobotoFont(
-                                      fontSize: 11,
-                                      fontWeight: FontWeight.w600,
-                                      color: AppColor.white),
-                                )),
-                            const Spacer(),
-                            Container(
-                                padding: EdgeInsets.symmetric(
-                                  horizontal: AppSize.width(context) * 0.01,
-                                ),
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(20),
-                                    color: currentData['reminderMessage'] ==
-                                            "Post OPS Care"
-                                        ? AppColor.green.withOpacity(0.6)
-                                        : currentData['reminderMessage'] ==
-                                                "3rd Reminder Sent"
-                                            ? AppColor.red.withOpacity(0.6)
-                                            : AppColor.green.withOpacity(0.6)),
-                                child: Text(
-                                  currentData['reminderMessage'],
-                                  style: applyRobotoFont(
-                                      fontSize: 11,
-                                      fontWeight: FontWeight.w600,
-                                      color: AppColor.black),
-                                )),
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Row(
-                          children: [
-                            Image.asset(
-                              currentData['image'],
-                              height: 25,
-                              width: 25,
-                            ),
-                            const SizedBox(
-                              width: 5,
-                            ),
-                            Text(
-                              currentData['name'],
-                              style: applyRobotoFont(
-                                  fontSize: 14, fontWeight: FontWeight.w400),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'Eye Assessment',
-                                  style: applyRobotoFont(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w500),
-                                ),
-                                const SizedBox(
-                                  height: 2,
-                                ),
-                                Text(
-                                  currentData['appointmentType'],
-                                  style: applyRobotoFont(
-                                      fontSize: 12,
-                                      fontWeight: FontWeight.w500,
-                                      color: AppColor.grey),
-                                ),
-                              ],
-                            ),
-                            const Spacer(),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.end,
-                              children: [
-                                Text(
-                                  'Test ID: AT 010101',
-                                  style: applyRobotoFont(
-                                      fontSize: 12,
-                                      fontWeight: FontWeight.w500,
-                                      color: AppColor.grey),
-                                ),
-                                const SizedBox(
-                                  height: 2,
-                                ),
-                                Text(
-                                  '19 Sep 2023',
-                                  style: applyRobotoFont(
-                                      fontSize: 12,
-                                      fontWeight: FontWeight.w500,
-                                      color: AppColor.grey),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 5,
-                        ),
                         Text(
-                          data[index]['MessageText'],
-                          softWrap: true,
+                          'Eye Assessment',
                           style: applyRobotoFont(
-                            fontSize: 14,
+                            fontSize: 16,
                             fontWeight: FontWeight.w500,
                           ),
                         ),
                         const SizedBox(
-                          height: 15,
+                          height: 2,
                         ),
-                        Row(
-                          children: [
-                            const SizedBox(
-                              width: 10,
-                            ),
-                            Expanded(
-                              flex: 1,
-                              child: InkWell(
-                                onTap: () {
-                                  Navigator.of(context).push(MaterialPageRoute(
-                                      builder: (context) =>
-                                          PatientAssessmentReportPage(
-                                            index: index,
-                                          )));
-                                },
-                                child: Text(
-                                  'View Report',
-                                  style: applyRobotoFont(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w500,
-                                      color: AppColor.primary),
-                                ),
-                              ),
-                            ),
-                            Expanded(
-                              flex: 2,
-                              child: InkWell(
-                                onTap: () {},
-                                child: Text(
-                                  'View Timeline',
-                                  style: applyRobotoFont(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w500,
-                                      color: AppColor.primary),
-                                ),
-                              ),
-                            ),
-                          ],
-                        )
+                        Text(
+                          currentData['appointmentType'],
+                          style: applyRobotoFont(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w500,
+                            color: AppColor.grey,
+                          ),
+                        ),
                       ],
                     ),
+                    const Spacer(),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          'Test ID: AT 010101',
+                          style: applyRobotoFont(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w500,
+                            color: AppColor.grey,
+                          ),
+                        ),
+                        const SizedBox(height: 2),
+                        Text(
+                          '19 Sep 2023',
+                          style: applyRobotoFont(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w500,
+                            color: AppColor.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  data[index]['MessageText'],
+                  softWrap: true,
+                  style: applyRobotoFont(
+                    fontSize: 14,
                   ),
                 ),
-              );
-            },
+                const SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  children: [
+                    const SizedBox(width: 10),
+                    InkWell(
+                      onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => PatientAssessmentReportPage(
+                              index: index,
+                            ),
+                          ),
+                        );
+                      },
+                      child: Text(
+                        'View Report',
+                        style: applyRobotoFont(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                          color: AppColor.primary,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 20),
+                    InkWell(
+                      onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                const PatientTestTimelinePage(),
+                          ),
+                        );
+                      },
+                      child: Text(
+                        'View Timeline',
+                        style: applyRobotoFont(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                          color: AppColor.primary,
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
