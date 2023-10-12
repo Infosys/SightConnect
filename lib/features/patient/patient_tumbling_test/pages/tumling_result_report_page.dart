60844f7b47fe4d750bff30efc1c229798a5f77bf
Sarthak Paranjpe <sarthak.paranjpe@infosys.com>
2023-10-12 11:44:37 +0530

    asd

--- lib/features/patient/patient_tumbling_test/pages/tumling_result_report_page.dart
+++ lib/features/patient/patient_tumbling_test/pages/tumling_result_report_page.dart
@@ -1,45 +1,47 @@
 import 'package:eye_care_for_all/core/constants/app_color.dart';
 import 'package:eye_care_for_all/core/constants/app_icon.dart';
 import 'package:eye_care_for_all/core/constants/app_size.dart';
-import 'package:eye_care_for_all/features/patient/patient_dashboard/presentation/providers/patient_dashboard_provider.dart';
+import 'package:eye_care_for_all/features/patient/patient_triage/presentation/patient_triage_eye_scan/widgets/eye_centre_details_cards.dart';
+import 'package:eye_care_for_all/features/patient/patient_triage/presentation/patient_triage_eye_scan/widgets/result_page_bottom_cards.dart';
+import 'package:eye_care_for_all/features/patient/patient_triage/presentation/patient_triage_eye_scan/widgets/result_page_top_card.dart';
 import 'package:eye_care_for_all/shared/theme/text_theme.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:syncfusion_flutter_gauges/gauges.dart';
-import 'package:url_launcher/url_launcher.dart';
-
-import '../providers/tumbling_test_provider.dart';
 
 class TumblingResultReportPage extends ConsumerWidget {
-  static const String routeName = "/tumbling-result-report";
-  const TumblingResultReportPage({super.key});
+  static const String routeName = "/patientEyesReportPage";
+  const TumblingResultReportPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
+      backgroundColor: AppColor.scaffold,
       appBar: AppBar(
-        title: const Text("Tumbling E Test - Completed"),
+        title: const Text("Eye Report"),
       ),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(AppSize.kmpadding),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
             children: [
-              const SizedBox(height: AppSize.kmheight),
+              const ResultPageTopCard(),
               Card(
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(AppSize.klradius),
                 ),
                 color: const Color(0xff333333),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
+                child: Row(
                   children: [
                     SizedBox(
-                      height: 230,
                       child: Stack(
                         clipBehavior: Clip.none,
-                        alignment: Alignment.topCenter,
+                        alignment: Alignment.centerLeft,
                         children: [
                           Container(
                             padding: const EdgeInsets.fromLTRB(
@@ -53,144 +55,111 @@ class TumblingResultReportPage extends ConsumerWidget {
                               borderRadius:
                                   BorderRadius.circular(AppSize.klradius),
                             ),
-                            height: 230,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  "Done! According to the test you don’t have visual acuity problems.",
-                                  textAlign: TextAlign.center,
-                                  style: applyRobotoFont(
-                                    fontSize: 14,
-                                    color: const Color(0xff333333),
-                                  ),
-                                ),
-                                const SizedBox(
-                                  height: AppSize.ksheight,
-                                ),
-                                buildGuage()
-                              ],
+                            width: AppSize.width(context) * 0.65,
+                            height: AppSize.height(context) * 0.22,
+                            child: Transform.translate(
+                              offset: const Offset(0, 28),
+                              child: buildGuage(),
                             ),
                           ),
-                          Positioned(
-                            top: -40,
-                            child: Card(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(200),
-                              ),
-                              child: const Padding(
-                                padding: EdgeInsets.all(14.0),
-                                child: Icon(
-                                  Icons.check_circle_outline,
-                                  color: AppColor.green,
-                                  size: 40,
-                                ),
-                              ),
-                            ),
-                          )
                         ],
                       ),
                     ),
-                    Consumer(
-                      builder: (context, ref, child) {
-                        var model = ref.watch(tumblingTestProvider);
-                        var leftEye = model.calculateLeftEyeSigth();
-
-                        var rightEye = model.calculateRightEyeSigth();
-                        return Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-                            buildCol(
-                              "Left Eye",
-                              leftEye.toString(),
-                            ),
-                            buildCol(
-                              "Right Eye",
-                              rightEye.toString(),
-                            ),
-                          ],
-                        );
-                      },
-                    )
+                    SizedBox(
+                      height: AppSize.height(context) * 0.22,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          buildCol('Left Eye', '1.0', AppColor.green),
+                          buildCol('Right Eye', '0.8', AppColor.orange),
+                          buildCol('Both Eye', '1.0', AppColor.green),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ),
               const SizedBox(
                 height: AppSize.kmheight,
               ),
-              const Text(
-                "For more details and insights, visit nearest LVPEI vision centre, or call our toll free number to connect with our vision technician.",
-              ),
-              ListTile(
-                onTap: () async {
-                  await launchOut(
-                    Uri(
-                      scheme: "geo",
-                      path: "15.8145,79.9920",
+              Container(
+                padding: const EdgeInsets.all(AppSize.kspadding),
+                decoration: BoxDecoration(
+                  color: AppColor.white,
+                  boxShadow: [
+                    BoxShadow(
+                      offset: const Offset(
+                        2,
+                        10,
+                      ),
+                      color: AppColor.primary.withOpacity(0.1),
+                      blurRadius: 20,
+                      spreadRadius: 5,
                     ),
-                  );
-                },
-                leading: SvgPicture.asset(
-                  AppIcon.visionCenter,
+                  ],
+                  borderRadius: BorderRadius.circular(AppSize.ksradius),
                 ),
-                trailing: SvgPicture.asset(
-                  AppIcon.location,
-                ),
-                title: const Text("LVPEI Vision Center"),
-                subtitle: const Text("Addanki"),
-              ),
-              ListTile(
-                onTap: () async {
-                  await launchOut(
-                    Uri(
-                      scheme: "tel",
-                      path: "08069490400",
-                    ),
-                  );
-                },
-                leading: SvgPicture.asset(
-                  AppIcon.tollFree,
-                ),
-                trailing: SvgPicture.asset(
-                  AppIcon.call,
-                ),
-                title: const Text("Vision Technician"),
-                subtitle: const Text("Toll Free Number"),
-              ),
-              const SizedBox(
-                height: AppSize.kmheight,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
                 child: Row(
+                  mainAxisSize: MainAxisSize.min,
                   children: [
-                    Expanded(
-                      child: ElevatedButton(
-                        onPressed: () {
-                          ref.read(patientDashboardProvider).changeIndex(0);
-                          Navigator.of(context)
-                              .popUntil((route) => route.isFirst);
-                        },
-                        child: const Text("Home"),
+                    CircleAvatar(
+                      backgroundColor: AppColor.primary.withOpacity(0.1),
+                      child: SvgPicture.asset(
+                        AppIcon.report,
+                        height: 16,
                       ),
                     ),
-                    const SizedBox(
-                      width: AppSize.kmheight,
+                    SizedBox(
+                      width: AppSize.width(context) * 0.05,
                     ),
-                    Expanded(
-                      child: OutlinedButton(
-                        onPressed: () {
-                          Navigator.of(context)
-                              .popUntil((route) => route.isFirst);
-                        },
-                        child: const Text("Test Again"),
+                    Text(
+                      "Eye Assessment Report",
+                      style: applyRobotoFont(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600,
                       ),
                     ),
+                    const Spacer(),
+                    const Icon(Icons.chevron_right_sharp),
                   ],
                 ),
               ),
+              const SizedBox(height: AppSize.klheight),
+              Text(
+                'Visit the nearest vision center for more details. Call the toll-free number to speak to our vision technician.',
+                textAlign: TextAlign.left,
+                softWrap: true,
+                style: applyRobotoFont(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              const SizedBox(height: AppSize.klheight),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Nearby Vision Centres",
+                    style: applyFiraSansFont(
+                      fontSize: 18,
+                      color: Colors.black,
+                    ),
+                  ),
+                  const Text(
+                    "See All",
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: AppColor.primary,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: AppSize.kmheight),
+              const EyeCentreDetailsCards(),
+              const SizedBox(height: AppSize.kmheight),
+              const ResultPageBottomCards(),
             ],
           ),
         ),
@@ -198,29 +167,20 @@ class TumblingResultReportPage extends ConsumerWidget {
     );
   }
 
-  Future<void> launchOut(Uri uri) async {
-    if (!await launchUrl(uri)) {
-      throw Exception('Could not launch $uri');
-    }
-  }
-
-  buildCol(String title, String value) {
+  buildCol(String title, String value, Color color) {
     return Padding(
-      padding: const EdgeInsets.all(16.0),
+      padding: const EdgeInsets.only(left: 10.0),
       child: Column(
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
             title,
-            style: applyFiraSansFont(fontSize: 18, color: AppColor.white),
-          ),
-          const SizedBox(
-            height: AppSize.ksheight,
+            style: applyFiraSansFont(fontSize: 12, color: AppColor.white),
           ),
           Text(
             value,
-            style: applyFiraSansFont(fontSize: 24, color: AppColor.green),
+            style: applyFiraSansFont(fontSize: 18, color: color),
           ),
         ],
       ),
@@ -228,48 +188,46 @@ class TumblingResultReportPage extends ConsumerWidget {
   }
 
   buildGuage() {
-    return Flexible(
-      child: SizedBox(
-        height: 140,
-        child: SfRadialGauge(
-          axes: [
-            RadialAxis(
-              showLabels: false,
-              showAxisLine: false,
-              showTicks: false,
-              minimum: 0,
-              maximum: 99,
-              ranges: <GaugeRange>[
-                GaugeRange(
-                  startValue: 0,
-                  endValue: 33,
-                  color: const Color(0xFFFE2A25),
-                  sizeUnit: GaugeSizeUnit.factor,
-                  startWidth: 0.2,
-                  endWidth: 0.2,
-                ),
-                GaugeRange(
-                  startValue: 33,
-                  endValue: 66,
-                  color: const Color(0xFFFFBA00),
-                  startWidth: 0.2,
-                  endWidth: 0.2,
-                  sizeUnit: GaugeSizeUnit.factor,
-                ),
-                GaugeRange(
-                  startValue: 66,
-                  endValue: 99,
-                  color: const Color(0xFF00AB47),
-                  sizeUnit: GaugeSizeUnit.factor,
-                  startWidth: 0.2,
-                  endWidth: 0.2,
-                ),
-              ],
-              pointers: const <GaugePointer>[NeedlePointer(value: 80)],
-            )
+    return SfRadialGauge(
+      axes: [
+        RadialAxis(
+          radiusFactor: 1.3,
+          startAngle: 180,
+          endAngle: 0,
+          showLabels: false,
+          showAxisLine: false,
+          showTicks: false,
+          minimum: 0,
+          maximum: 99,
+          ranges: <GaugeRange>[
+            GaugeRange(
+              startValue: 0,
+              endValue: 33,
+              color: const Color(0xFFFE2A25),
+              sizeUnit: GaugeSizeUnit.factor,
+              startWidth: 0.2,
+              endWidth: 0.2,
+            ),
+            GaugeRange(
+              startValue: 33,
+              endValue: 66,
+              color: const Color(0xFFFFBA00),
+              startWidth: 0.2,
+              endWidth: 0.2,
+              sizeUnit: GaugeSizeUnit.factor,
+            ),
+            GaugeRange(
+              startValue: 66,
+              endValue: 99,
+              color: const Color(0xFF00AB47),
+              sizeUnit: GaugeSizeUnit.factor,
+              startWidth: 0.2,
+              endWidth: 0.2,
+            ),
           ],
-        ),
-      ),
+          pointers: const <GaugePointer>[NeedlePointer(value: 80)],
+        )
+      ],
     );
   }
 }
