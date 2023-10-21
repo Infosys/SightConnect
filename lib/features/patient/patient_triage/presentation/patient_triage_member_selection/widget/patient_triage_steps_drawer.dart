5c1c69e1766d7eb45fbad4f479f201685c36e1d0
Sarthak Paranjpe <sarthak.paranjpe@infosys.com>
2023-10-22 00:24:48 +0530

    made a new stepper

--- lib/features/patient/patient_triage/presentation/patient_triage_member_selection/widget/patient_triage_steps_drawer.dart
+++ lib/features/patient/patient_triage/presentation/patient_triage_member_selection/widget/patient_triage_steps_drawer.dart
@@ -1,3 +1,4 @@
+import 'package:easy_stepper/easy_stepper.dart';
 import 'package:eye_care_for_all/core/constants/app_color.dart';
 import 'package:eye_care_for_all/core/constants/app_size.dart';
 import 'package:eye_care_for_all/features/patient/patient_triage/presentation/providers/patient_triage_stepper_provider.dart';
@@ -24,6 +25,7 @@ class PatientTriageStepsDrawer extends ConsumerWidget {
         padding: const EdgeInsets.all(8.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
             const SizedBox(height: AppSize.klheight),
             Row(
@@ -41,77 +43,200 @@ class PatientTriageStepsDrawer extends ConsumerWidget {
                     )),
               ],
             ),
-            Stepper(
-              currentStep: model.currentStep > 3 ? 3 : model.currentStep,
-              stepIconBuilder: (context, index) {
-                if (index == StepState.complete) {
-                  return Container(
-                      decoration: BoxDecoration(
-                        color: AppColor.primary,
-                        borderRadius: BorderRadius.circular(40),
-                        border: Border.all(
-                          color: Colors.grey,
+            Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                // alignment: Alignment.topLeft,
+                // fit: StackFit.loose,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(top: AppSize.klheight + 2),
+                    child: EasyStepper(
+                      disableScroll: true,
+                      fitWidth: true,
+                      borderThickness: 3,
+                      lineStyle: const LineStyle(
+                          lineType: LineType.normal,
+                          lineThickness: 5,
+                          unreachedLineColor: AppColor.altGrey,
+                          activeLineColor: AppColor.altGrey,
+                          finishedLineColor: AppColor.green,
+                          lineLength: 50),
+                      stepShape: StepShape.rRectangle,
+                      stepBorderRadius: 100,
+                      defaultStepBorderType: BorderType.normal,
+                      enableStepTapping: false,
+                      direction: Axis.vertical,
+                      unreachedStepBorderType: BorderType.normal,
+                      unreachedStepIconColor: AppColor.grey,
+                      unreachedStepBorderColor: AppColor.altGrey,
+                      unreachedStepBackgroundColor: AppColor.white,
+                      activeStepIconColor: AppColor.white,
+                      activeStepBackgroundColor: AppColor.blue,
+                      finishedStepBackgroundColor: AppColor.green,
+                      finishedStepIconColor: AppColor.white,
+                      activeStep: model.currentStep,
+                      activeStepTextColor: Colors.black87,
+                      finishedStepTextColor: Colors.black87,
+                      internalPadding: 0,
+                      showLoadingAnimation: false,
+                      padding: const EdgeInsets.all(0),
+                      stepRadius: 14,
+                      showStepBorder: true,
+                      steps: const [
+                        EasyStep(
+                          icon: Icon(
+                            Icons.more_horiz,
+                            size: 25,
+                            weight: 10,
+                          ),
+                          activeIcon: Icon(
+                            Icons.more_horiz,
+                            size: 25,
+                            weight: 10,
+                          ),
+                          finishIcon: Icon(
+                            Icons.check,
+                            size: 25,
+                            weight: 10,
+                          ),
                         ),
-                      ),
-                      child: const Icon(
-                        Icons.check,
-                        color: Colors.white,
-                        size: 20,
-                      ));
-                } else {
-                  return Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(40),
-                      border: Border.all(color: Colors.grey),
+                        EasyStep(
+                          icon: Icon(
+                            Icons.more_horiz,
+                            size: 25,
+                            weight: 10,
+                          ),
+                          activeIcon: Icon(
+                            Icons.more_horiz,
+                            size: 25,
+                            weight: 10,
+                          ),
+                          finishIcon: Icon(
+                            Icons.check,
+                            size: 25,
+                            weight: 10,
+                          ),
+                        ),
+                        EasyStep(
+                          icon: Icon(
+                            Icons.more_horiz,
+                            size: 25,
+                            weight: 10,
+                          ),
+                          activeIcon: Icon(
+                            Icons.more_horiz,
+                            size: 25,
+                            weight: 10,
+                          ),
+                          finishIcon: Icon(
+                            Icons.check,
+                            size: 25,
+                            weight: 10,
+                          ),
+                        ),
+                        EasyStep(
+                          icon: Icon(
+                            Icons.more_horiz,
+                            size: 25,
+                            weight: 10,
+                          ),
+                          activeIcon: Icon(
+                            Icons.more_horiz,
+                            size: 25,
+                            weight: 10,
+                          ),
+                          finishIcon: Icon(
+                            Icons.check,
+                            size: 25,
+                            weight: 10,
+                          ),
+                        ),
+                      ],
+                      onStepReached: (index) => model.nextStep(index),
                     ),
-                    child: const Icon(
-                      Icons.more_horiz_outlined,
-                      color: Colors.grey,
-                      size: 20,
-                    ),
-                  );
-                }
-              },
-              controlsBuilder: (context, controller) {
-                return Container();
-              },
-              steps: [
-                Step(
-                  state: model.currentStep > 0
-                      ? StepState.complete
-                      : StepState.editing,
-                  title: const Text("Step 1 - Eye Assessment Questions"),
-                  subtitle: const Text("On Going"),
-                  content: const Text(""),
-                ),
-                Step(
-                  state: model.currentStep > 1
-                      ? StepState.complete
-                      : StepState.editing,
-                  title: const Text("Step 2 - Visual Acuity Test"),
-                  subtitle: const Text("Next Step"),
-                  content: const Text(""),
-                ),
-                Step(
-                  state: model.currentStep > 2
-                      ? StepState.complete
-                      : StepState.editing,
-                  title: const Text("Step 3 - Eye Scan"),
-                  subtitle: const Text("Pending"),
-                  content: const Text(""),
-                ),
-                Step(
-                  state: model.currentStep > 3
-                      ? StepState.complete
-                      : StepState.editing,
-                  title: const Text("Assessment Results"),
-                  subtitle: const Text("Complete all steps to view results"),
-                  content: const Text(""),
-                ),
-              ],
-            ),
-            const SizedBox(height: AppSize.klheight),
+                  ),
+                  SizedBox(
+                    width: AppSize.width(context) * 0.65,
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Text(
+                            "Step 1 - Eye Assessment Questions",
+                            softWrap: true,
+                            style: applyRobotoFont(
+                                fontSize: 14, fontWeight: FontWeight.w500),
+                          ),
+                          Text(
+                            model.currentStep == 0
+                                ? "ongoing"
+                                : model.currentStep > 0
+                                    ? "completed"
+                                    : "pending",
+                            softWrap: true,
+                            style: applyRobotoFont(
+                                fontSize: 12,
+                                color: AppColor.altGrey,
+                                fontWeight: FontWeight.w500),
+                          ),
+                          const SizedBox(
+                            height: AppSize.klheight * 3.5,
+                          ),
+                          Text(
+                            "Step 2 - Visual Acuity Test",
+                            softWrap: true,
+                            style: applyRobotoFont(
+                                fontSize: 14, fontWeight: FontWeight.w500),
+                          ),
+                          Text(
+                            model.currentStep == 1
+                                ? "ongoing"
+                                : model.currentStep > 1
+                                    ? "completed"
+                                    : "pending",
+                            softWrap: true,
+                            style: applyRobotoFont(
+                                fontSize: 12,
+                                color: AppColor.altGrey,
+                                fontWeight: FontWeight.w500),
+                          ),
+                          const SizedBox(
+                            height: AppSize.klheight * 3.5,
+                          ),
+                          Text(
+                            "Step 3 - Eye Scan",
+                            style: applyRobotoFont(
+                                fontSize: 14, fontWeight: FontWeight.w500),
+                          ),
+                          Text(
+                            model.currentStep == 2
+                                ? "ongoing"
+                                : model.currentStep > 2
+                                    ? "completed"
+                                    : "pending",
+                            style: applyRobotoFont(
+                                fontSize: 12,
+                                color: AppColor.altGrey,
+                                fontWeight: FontWeight.w500),
+                          ),
+                          const SizedBox(
+                            height: AppSize.klheight * 3.4,
+                          ),
+                          Text(
+                            "Assessment Results",
+                            style: applyRobotoFont(
+                                fontSize: 14, fontWeight: FontWeight.w500),
+                          ),
+                          Text(
+                            "Complete all steps to view results",
+                            style: applyRobotoFont(
+                                fontSize: 12,
+                                color: AppColor.altGrey,
+                                fontWeight: FontWeight.w500),
+                          ),
+                        ]),
+                  ),
+                ]),
+            const SizedBox(height: AppSize.klheight + 10),
             Row(
               children: [
                 const SizedBox(
