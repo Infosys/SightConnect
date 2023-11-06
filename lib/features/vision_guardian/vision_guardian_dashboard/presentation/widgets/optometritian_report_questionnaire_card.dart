c15cf56ebf04eed9a1f568e34aa79b517fd534df
Avendra Singh Gaur <avendrasingh.gaur@ad.infosys.com>
2023-11-06 17:31:34 +0530

    updates

--- lib/features/vision_guardian/vision_guardian_dashboard/presentation/widgets/optometritian_report_questionnaire_card.dart
+++ lib/features/vision_guardian/vision_guardian_dashboard/presentation/widgets/optometritian_report_questionnaire_card.dart
@@ -1,5 +1,6 @@
 import 'package:eye_care_for_all/core/constants/app_color.dart';
 import 'package:eye_care_for_all/core/constants/app_size.dart';
+import 'package:eye_care_for_all/features/patient/patient_triage/presentation/patient_triage_questionnaire/provider/patient_triage_questionnaire_provider.dart';
 import 'package:eye_care_for_all/features/vision_guardian/vision_guardian_dashboard/presentation/provider/optometritian_report_provider.dart';
 import 'package:eye_care_for_all/shared/theme/text_theme.dart';
 import 'package:flutter/material.dart';
@@ -12,27 +13,9 @@ class OptometritianReportQuestionnaireCard extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     TriageUrgency urgency =
         ref.watch(optometricianReportProvider).calculateUrgency();
-    List<Map<String, dynamic>> data = [
-      {
-        "question": "Are you facing sudden loss of vision?",
-        "answer": ["No"]
-      },
-      {
-        "question": "2. Is your vision not clear or disturbed?",
-        "answer": [
-          "I see halos or colored rings around lights",
-          "I have pain or discomfort when viewing bright light"
-        ]
-      },
-      {
-        "question":
-            "3. 1. Are you experiencing any of the below problems in your eyes?",
-        "answer": [
-          "My eyes appear wide open and bulged out",
-          "Others - My eyelids twitch uncontrollably with jerky contractions"
-        ]
-      },
-    ];
+    List<Map<String, dynamic>> data = ref
+        .watch(patientTriageQuestionnaireProvider)
+        .questionnaireForReportPage;
 
     return Container(
       color: AppColor.white,
@@ -151,7 +134,11 @@ class OptometritianReportQuestionnaireCard extends ConsumerWidget {
                 ),
                 child: Center(
                   child: Text(
-                    'Urgent Consult',
+                    urgency == TriageUrgency.EMERGENCY
+                        ? 'Urgent Consult'
+                        : urgency == TriageUrgency.PRIORITY
+                            ? 'Early Consult'
+                            : 'Regular Consult',
                     style: applyRobotoFont(
                       fontSize: 12,
                       color: AppColor.white,
