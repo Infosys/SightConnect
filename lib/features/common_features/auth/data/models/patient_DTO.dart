6e74549961ce258128cf801e4e5d1b789994fe45
Avinash Bonthu <avinash.bonthu@infosys.com>
2023-11-28 17:18:21 +0530

    added patient registration models

---
+++ lib/features/common_features/auth/data/models/patient_DTO.dart
@@ -0,0 +1,35 @@
+import 'package:eye_care_for_all/features/common_features/auth/data/enums/gender.dart';
+import 'package:eye_care_for_all/features/common_features/auth/data/enums/relationship.dart';
+import 'package:eye_care_for_all/features/common_features/auth/data/models/address_DTO.dart';
+import 'package:eye_care_for_all/features/common_features/auth/data/models/location_DTO.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'patient_DTO.freezed.dart';
+part 'patient_DTO.g.dart';
+
+@freezed
+class PatientDTO with _$PatientDTO {
+  const factory PatientDTO({
+    String? counterId,
+    String? hprID,
+    String? hipCode,
+    LocationDTO? location,
+    String? abhaId,
+    String? abhaAddress,
+    String? name,
+    Gender? gender,
+    String? mobile,
+    String? email,
+    String? profilePhoto,
+    String? yearOfBirth,
+    double? monthOfBirth,
+    double? dayOfBirth,
+    int? age,
+    String? parentPatientId,
+    Relationship? relationship,
+    List<AddressDTO>? address
+  }) = _PatientDTO;
+
+  factory PatientDTO.fromJson(Map<String, dynamic> json) =>
+      _$PatientDTOFromJson(json); 
+}
