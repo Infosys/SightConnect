1f56cc57d038c9d812db53999ba24f20da3bc8ca
Mayur Madhwani <mayur.madhwani@infosys.com>
2023-12-14 15:43:55 +0530

    changes

--- lib/features/vision_technician/vision_technician_register_new_patient/presentation/providers/register_new_patient_helper_provider.dart
+++ lib/features/vision_technician/vision_technician_register_new_patient/presentation/providers/register_new_patient_helper_provider.dart
@@ -1,7 +1,7 @@
 import 'dart:math';
 
 import 'package:eye_care_for_all/features/vision_technician/vision_technician_home/data/models/vt_patient_model.dart';
-import 'package:eye_care_for_all/features/vision_technician/vision_technician_home/data/repositories/vision_technician_home_repository_impl.dart';
+import 'package:eye_care_for_all/features/vision_technician/vision_technician_home/domain/repositories/vision_technician_home_repository_impl.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
