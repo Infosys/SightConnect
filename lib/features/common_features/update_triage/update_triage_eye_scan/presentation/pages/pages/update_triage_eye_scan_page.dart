7c4f125f88dcfc34e78ad62e65d98906634864a8
Avendra Singh Gaur <avendrasingh.gaur@ad.infosys.com>
2023-12-18 00:53:01 +0530

    updates

---
+++ lib/features/common_features/update_triage/update_triage_eye_scan/presentation/pages/pages/update_triage_eye_scan_page.dart
@@ -0,0 +1,11 @@
+import 'package:eye_care_for_all/features/common_features/update_triage/update_triage_eye_scan/presentation/pages/pages/update_triage_eye_scan_instructions_page.dart';
+import 'package:flutter/material.dart';
+
+class UpdateTriageEyeScanPage extends StatelessWidget {
+  const UpdateTriageEyeScanPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const UpdateTriageEyeScanInstructions();
+  }
+}
