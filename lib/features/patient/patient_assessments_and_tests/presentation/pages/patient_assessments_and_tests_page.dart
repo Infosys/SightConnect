2501511f70823e496b60bfd75cb1297222d79bab
Mahavir Singh <mahavir.singh01@infosys.com>
2023-12-19 22:34:12 +0530

    update question tested

--- lib/features/patient/patient_assessments_and_tests/presentation/pages/patient_assessments_and_tests_page.dart
+++ lib/features/patient/patient_assessments_and_tests/presentation/pages/patient_assessments_and_tests_page.dart
@@ -4,18 +4,30 @@ import 'package:eye_care_for_all/core/constants/app_size.dart';
 import 'package:eye_care_for_all/features/patient/patient_assessments_and_tests/domain/entities/triage_report_brief_entity.dart';
 import 'package:eye_care_for_all/features/patient/patient_assessments_and_tests/presentation/provider/patient_assessments_and_test_provider.dart';
 import 'package:eye_care_for_all/features/patient/patient_assessments_and_tests/presentation/widgets/assements_cards.dart';
+import 'package:eye_care_for_all/main.dart';
 import 'package:eye_care_for_all/shared/theme/text_theme.dart';
 import 'package:eye_care_for_all/shared/widgets/custom_app_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class AssessmentsAndTestsPage extends HookConsumerWidget {
   const AssessmentsAndTestsPage({super.key});
+  
+    
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+
+    
     var model = ref.watch(assessmentsAndTestProvider);
+    useEffect(() {
+      logger.f("use effect called");
+      ref.refresh(getEyeTriageReport(model.selectedUser.id));
+      return null; // Cleanup function, will be called when widget is disposed
+    }, [model.selectedUser]);
+    
     return Scaffold(
       appBar: const CustomAppbar(
         title: Text("Assessments and Tests"),
@@ -138,6 +150,7 @@ class AssessmentsAndTestsPage extends HookConsumerWidget {
             const SizedBox(height: AppSize.kmheight),
             Consumer(
               builder: (context, ref, child) {
+              
                 return ref
                     .watch(getEyeTriageReport(model.selectedUser.id))
                     .when(
