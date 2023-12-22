019ecb2b61322fa3652c098d3ff29120043c1c86
Nandan Reddy <nandan.reddy@infosys.com>
2023-12-22 19:37:17 +0530

    Add DeepLinking Support

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:eye_care_for_all/app_environment.dart';
 import 'package:eye_care_for_all/core/services/app_logger.dart';
 import 'package:eye_care_for_all/core/services/ios_device_info_service.dart';
 import 'package:eye_care_for_all/core/services/persistent_auth_service.dart';
+import 'package:eye_care_for_all/init_platform_state.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:logger/logger.dart';
@@ -13,6 +14,7 @@ Logger logger = Logger();
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   HttpOverrides.global = MyHttpOverrides();
+  initPlatformState();
   await PersistentAuthStateService.intializeAuth();
   await SharedPreferenceService.init();
 
