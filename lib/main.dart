06268461fa9c2cd7a3abc2f5982df9f613e2e65d
Abhishek Singh(abhishek.singh169) <abhishek.singh169@infosys.com>
2024-01-02 22:20:33 +0530

    Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'dart:io';
+
 import 'package:eye_care_for_all/app/app.dart';
 import 'package:eye_care_for_all/app_environment.dart';
 import 'package:eye_care_for_all/core/services/ios_device_info_service.dart';
 import 'package:eye_care_for_all/core/services/persistent_auth_service.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_miniapp_web_runner/flutter_miniapp_web_runner.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:logger/logger.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'core/services/app_logger.dart';
 import 'core/services/shared_preference.dart';
 
@@ -16,7 +21,9 @@ Future<void> main() async {
   await PersistentAuthStateService.intializeAuth();
   await SharedPreferenceService.init();
   IOSDeviceInfoService.init();
-
+  if (!kIsWeb && defaultTargetPlatform == TargetPlatform.android) {
+    await InAppWebViewController.setWebContentsDebuggingEnabled(kDebugMode);
+  }
   if (AppEnv.isProd) {
     await AppLogger.init();
   }
@@ -28,18 +35,7 @@ Future<void> main() async {
   );
 }
 
-// Future<void> requestPermission() async {
-//   final permissions = [
-//     Permission.camera,
-//     Permission.storage,
-//   ];
-//   final statuses = await permissions.request();
-//   if (statuses[Permission.camera] == PermissionStatus.denied ||
-//       statuses[Permission.storage] == PermissionStatus.denied) {
-//   } else {
-//     logger.i("Permission granted");
-//   }
-// }
+
 
 // class MyHttpOverrides extends HttpOverrides {
 //   @override
