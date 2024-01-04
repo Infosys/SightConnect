6ffe7e43c57de3118e71c2b99d2755d32622ca7e
Nandan Reddy <nandan.reddy@infosys.com>
2024-01-04 16:35:18 +0530

    Update `app_en.arb` for app_drawer.
    Made some changes in `drawer_menu_item.dart` in order to accommodate the localization of the `app_drawer_page`.
    
    Initially all the items that are supposed to be shown in the app_drawer_page are being loaded from `drawer_menu_item` using `DrawerMenuItems.all`.
    
    ### Modifications of `DrawerMenuItems` class
    - Removed all the static `DrawerMenuItem`s along with the  static `all` variable.
    - Added a method called `getAll` which takes a single argument `loc` of type `AppLocalizations` and returns a list of `DrawerMenuItem` with the title in the user preffered language (for this we need `loc`).
    
    ### Modifications of `DrawerMenuItem` class
    - Added a property called `id` which is an enum of type `DraweMenuItemId`. This can be used to uniquely identify each `DrawerMenuItem` as we cannot rely of the `title` which can change based on the preffered language.
    
    ### Modifications in `app_drawer.dart`
    - Replaced `items.asMap().entries` with just `items`.
    - Instead of relying on `item.key` (previously), we can now rely on `item.id` to perform appropriate action inside `onTap`. This will especially be useful when the order of items is changed as the `id` remains constant whereas the `key` will not remain so.

--- lib/core/models/drawer_menu_item.dart
+++ lib/core/models/drawer_menu_item.dart
@@ -1,65 +1,79 @@
 import 'package:eye_care_for_all/core/constants/app_icon.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+enum DrawerMenuItemId {
+  home,
+  profile,
+  // appointments,
+  prescriptions,
+  scanner,
+  // accessibilities,
+  about,
+  // termOfUse,
+  privacyPolicy,
+  help,
+}
 
 class DrawerMenuItem {
+  final DrawerMenuItemId id;
   final String title;
   final String icon;
   const DrawerMenuItem({
+    required this.id,
     required this.title,
     required this.icon,
   });
 }
 
 class DrawerMenuItems {
-  static const DrawerMenuItem home = DrawerMenuItem(
-    title: "Home",
-    icon: AppIcon.drawerHome,
-  );
-  static const DrawerMenuItem profile = DrawerMenuItem(
-    title: "My Profile",
-    icon: AppIcon.navProfile,
-  );
-  // static const DrawerMenuItem appointments = DrawerMenuItem(
-  //   title: "Appointments",
-  //   icon: AppIcon.drawerAppoinments,
-  // );
-  static const DrawerMenuItem prescriptions = DrawerMenuItem(
-    title: "Assessments and Tests",
-    icon: AppIcon.drawerAssessmentsAndTest,
-  );
-  static const DrawerMenuItem scanner = DrawerMenuItem(
-    title: "Scanner",
-    icon: AppIcon.scanner,
-  );
-  // static const DrawerMenuItem accessibilities = DrawerMenuItem(
-  //   title: "Accessibility Settings",
-  //   icon: AppIcon.drawerAssessibility,
-  // );
-  static const DrawerMenuItem about = DrawerMenuItem(
-    title: "About Us",
-    icon: AppIcon.drawerAboutUs,
-  );
-  // static const DrawerMenuItem termOfUse = DrawerMenuItem(
-  //   title: "Terms of Use",
-  //   icon: AppIcon.drawerTermOfUse,
-  // );
-  static const DrawerMenuItem privacyPolicy = DrawerMenuItem(
-    title: "Privacy Policy",
-    icon: AppIcon.drawerPrivacyPolicy,
-  );
-  static const DrawerMenuItem help = DrawerMenuItem(
-    title: "Help and Support",
-    icon: AppIcon.drawerHelpAndSupport,
-  );
-  static const List<DrawerMenuItem> all = [
-    home,
-    profile,
-    // appointments,
-    prescriptions,
-    scanner,
-    // accessibilities,
-    about,
-    // termOfUse,
-    privacyPolicy,
-    help,
-  ];
+  static List<DrawerMenuItem> getAll(AppLocalizations loc) {
+    DrawerMenuItem home = DrawerMenuItem(
+      id: DrawerMenuItemId.home,
+      title: loc.appDrawerHome,
+      icon: AppIcon.drawerHome,
+    );
+    DrawerMenuItem profile = DrawerMenuItem(
+      id: DrawerMenuItemId.profile,
+      title: loc.appDrawerMyProfile,
+      icon: AppIcon.navProfile,
+    );
+    DrawerMenuItem prescriptions = DrawerMenuItem(
+      id: DrawerMenuItemId.prescriptions,
+      title: loc.appDrawerAssessmentsAndTests,
+      icon: AppIcon.drawerAssessmentsAndTest,
+    );
+    DrawerMenuItem scanner = DrawerMenuItem(
+      id: DrawerMenuItemId.scanner,
+      title: loc.appDrawerScanner,
+      icon: AppIcon.scanner,
+    );
+    DrawerMenuItem about = DrawerMenuItem(
+      id: DrawerMenuItemId.about,
+      title: loc.appDrawerAboutUs,
+      icon: AppIcon.drawerAboutUs,
+    );
+    DrawerMenuItem privacyPolicy = DrawerMenuItem(
+      id: DrawerMenuItemId.privacyPolicy,
+      title: loc.appDrawerPrivacyPolicy,
+      icon: AppIcon.drawerPrivacyPolicy,
+    );
+    DrawerMenuItem help = DrawerMenuItem(
+      id: DrawerMenuItemId.help,
+      title: loc.appDrawerHelpAndSupport,
+      icon: AppIcon.drawerHelpAndSupport,
+    );
+
+    return [
+      home,
+      profile,
+      // appointments,
+      prescriptions,
+      scanner,
+      // accessibilities,
+      about,
+      // termOfUse,
+      privacyPolicy,
+      help,
+    ];
+  }
 }
--- lib/shared/widgets/app_drawer.dart
+++ lib/shared/widgets/app_drawer.dart
@@ -8,6 +8,7 @@ import 'package:eye_care_for_all/features/common_features/initialization/provide
 import 'package:eye_care_for_all/features/patient/patient_assessments_and_tests/presentation/pages/patient_assessments_and_tests_page.dart';
 
 import 'package:eye_care_for_all/features/patient/patient_profile/presentation/pages/patient_profile_page.dart';
+import 'package:eye_care_for_all/shared/extensions/widget_extension.dart';
 import 'package:eye_care_for_all/shared/theme/text_theme.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -26,7 +27,8 @@ class AppDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var items = DrawerMenuItems.all;
+    final loc = context.loc!;
+    var items = DrawerMenuItems.getAll(loc);
     return Scaffold(
       backgroundColor: AppColor.primary,
       body: Padding(
@@ -50,7 +52,7 @@ class AppDrawer extends StatelessWidget {
                     color: AppColor.white,
                   ),
                   title: Text(
-                    "Menu",
+                    loc.appDrawerTitle,
                     style: applyFiraSansFont(
                       color: AppColor.white,
                       fontWeight: FontWeight.w500,
@@ -63,110 +65,108 @@ class AppDrawer extends StatelessWidget {
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        ...items.asMap().entries.map(
-                              (item) => ListTile(
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(20),
-                                ),
-                                splashColor: AppColor.blue,
-                                onTap: () {
-                                  switch (item.key) {
-                                    case 0:
-                                      Navigator.of(context).pop();
-                                      break;
+                        ...items.map(
+                          (item) => ListTile(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            splashColor: AppColor.blue,
+                            onTap: () {
+                              switch (item.id) {
+                                case DrawerMenuItemId.home:
+                                  Navigator.of(context).pop();
+                                  break;
 
-                                    case 1:
-                                      Navigator.of(context).push(
-                                        MaterialPageRoute(
-                                          builder: (context) =>
-                                              const PatientProfilePage(),
-                                        ),
-                                      );
+                                case DrawerMenuItemId.profile:
+                                  Navigator.of(context).push(
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          const PatientProfilePage(),
+                                    ),
+                                  );
 
-                                    case 2:
-                                      Navigator.of(context).push(
-                                        MaterialPageRoute(
-                                          builder: (context) =>
-                                              const AssessmentsAndTestsPage(),
-                                        ),
-                                      );
-                                      break;
-                                    case 3:
-                                      Navigator.of(context).push(
-                                        MaterialPageRoute(
-                                          builder: (context) =>
-                                              const SuperAppScannerPage(),
-                                        ),
-                                      );
-                                      break;
-                                    case 4:
-                                      Navigator.of(context).push(
-                                        MaterialPageRoute(
-                                          builder: (context) =>
-                                              const AboutUsPage(),
-                                        ),
-                                      );
-                                      break;
+                                case DrawerMenuItemId.prescriptions:
+                                  Navigator.of(context).push(
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          const AssessmentsAndTestsPage(),
+                                    ),
+                                  );
+                                  break;
+                                case DrawerMenuItemId.scanner:
+                                  Navigator.of(context).push(
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          const SuperAppScannerPage(),
+                                    ),
+                                  );
+                                  break;
+                                case DrawerMenuItemId.about:
+                                  Navigator.of(context).push(
+                                    MaterialPageRoute(
+                                      builder: (context) => const AboutUsPage(),
+                                    ),
+                                  );
+                                  break;
 
-                                    case 5:
-                                      Navigator.of(context).push(
-                                        MaterialPageRoute(
-                                          builder: (context) =>
-                                              const PatientPrivacyPolicyPage(),
-                                        ),
-                                      );
-                                      break;
-                                    case 6:
-                                      Navigator.of(context).push(
-                                        MaterialPageRoute(
-                                          builder: (context) =>
-                                              const HelpAndSupportPage(
-                                            helpLine: AppText.tollFreeNumber,
-                                          ),
-                                        ),
-                                      );
-                                      break;
-                                    default:
-                                  }
-                                },
-                                visualDensity:
-                                    const VisualDensity(horizontal: -4),
-                                leading: item.value.icon.contains("svg")
-                                    ? SizedBox(
-                                        height: 20,
-                                        width: 20,
-                                        child: SvgPicture.asset(
-                                          item.value.icon,
-                                          colorFilter: const ColorFilter.mode(
-                                            AppColor.white,
-                                            BlendMode.srcIn,
-                                          ),
-                                        ),
-                                      )
-                                    : Container(
-                                        height: 40,
-                                        width: 40,
-                                        transform: Matrix4.translationValues(
-                                          -10,
-                                          0,
-                                          0,
-                                        ),
-                                        child: Image.asset(
-                                          item.value.icon,
-                                          alignment: Alignment.centerLeft,
-                                          fit: BoxFit.cover,
-                                        ),
+                                case DrawerMenuItemId.privacyPolicy:
+                                  Navigator.of(context).push(
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          const PatientPrivacyPolicyPage(),
+                                    ),
+                                  );
+                                  break;
+                                case DrawerMenuItemId.help:
+                                  Navigator.of(context).push(
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          const HelpAndSupportPage(
+                                        helpLine: AppText.tollFreeNumber,
                                       ),
-                                title: Text(
-                                  item.value.title,
-                                  style: applyFiraSansFont(
-                                    color: AppColor.white,
-                                    fontWeight: FontWeight.normal,
-                                    fontSize: 18,
+                                    ),
+                                  );
+                                  break;
+                                default:
+                              }
+                            },
+                            visualDensity: const VisualDensity(horizontal: -4),
+                            leading: item.icon.contains("svg")
+                                ? SizedBox(
+                                    height: 20,
+                                    width: 20,
+                                    child: SvgPicture.asset(
+                                      item.icon,
+                                      colorFilter: const ColorFilter.mode(
+                                        AppColor.white,
+                                        BlendMode.srcIn,
+                                      ),
+                                    ),
+                                  )
+                                : Container(
+                                    height: 40,
+                                    width: 40,
+                                    transform: Matrix4.translationValues(
+                                      -10,
+                                      0,
+                                      0,
+                                    ),
+                                    child: Image.asset(
+                                      item.icon,
+                                      alignment: Alignment.centerLeft,
+                                      fit: BoxFit.cover,
+                                    ),
                                   ),
-                                ),
+                            title: Text(
+                              item.title,
+                              style: applyFiraSansFont(
+                                color: AppColor.white,
+                                fontWeight: FontWeight.normal,
+                                fontSize: 18,
                               ),
                             ),
+                          ),
+                        ),
                         Consumer(
                           builder: (context, ref, _) => ListTile(
                             onTap: () async {
@@ -194,7 +194,7 @@ class AppDrawer extends StatelessWidget {
                               ),
                             ),
                             title: Text(
-                              "Sign Out",
+                              loc.appDrawerSignOut,
                               style: applyFiraSansFont(
                                 color: AppColor.white,
                                 fontWeight: FontWeight.normal,
