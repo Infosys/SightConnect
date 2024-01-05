e093964bcd49bcfb5977b0f39935b1ae6d335d80
Abhishek Singh(abhishek.singh169) <abhishek.singh169@infosys.com>
2024-01-05 15:26:22 +0530

    added

--- lib/core/services/dio_service.dart
+++ lib/core/services/dio_service.dart
@@ -1,5 +1,8 @@
+import 'dart:developer';
+
 import 'package:dio/dio.dart';
 import 'package:eye_care_for_all/app_environment.dart';
+import 'package:eye_care_for_all/core/providers/global_language_provider.dart';
 import 'package:eye_care_for_all/core/services/interceptors.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -20,12 +23,13 @@ var keycloakDioProvider = Provider(
 
 var dioProvider = Provider(
   (ref) {
+    final lang = ref.watch(globalLanguageProvider).currentLocale.languageCode;
     final dio = Dio(
       BaseOptions(
         baseUrl: AppEnv.baseUrl,
         connectTimeout: const Duration(seconds: 30),
         headers: {
-          "X-Accept-Language": "en",
+          "X-Accept-Language": lang,
         },
       ),
     );
