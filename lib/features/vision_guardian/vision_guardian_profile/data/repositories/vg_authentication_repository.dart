3ca382974c3d7303a4474cdd59ba31f7eca9787b
Abhishek Singh(abhishek.singh169) <abhishek.singh169@infosys.com>
2024-01-12 17:00:06 +0530

    added

--- lib/features/vision_guardian/vision_guardian_profile/data/repositories/vg_authentication_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:eye_care_for_all/core/services/failure.dart';
-import 'package:eye_care_for_all/features/vision_guardian/vision_guardian_profile/data/models/vg_profile_model.dart';
-
-abstract class VgAuthenticationRepository {
-  Future<Either<Failure, List<VgProfileModel>>> getVgProfile(String mobile);
-}
