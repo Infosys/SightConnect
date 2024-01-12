3e3f4f4233db02921145aeaf75c4f6c5d1c8d02a
Abhishek Singh(abhishek.singh169) <abhishek.singh169@infosys.com>
2024-01-12 12:29:35 +0530

    Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'dart:io';
 
 import 'package:eye_care_for_all/app/app.dart';
-import 'package:eye_care_for_all/app_environment.dart';
 import 'package:eye_care_for_all/core/services/ios_device_info_service.dart';
 import 'package:eye_care_for_all/core/services/persistent_auth_service.dart';
-import 'package:eye_care_for_all/core/services/sqflite_service.dart';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
