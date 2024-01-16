239674d81f6b8530f9e7c6604ce884d25cc468f0
Sarthak Paranjpe <sarthak.paranjpe@infosys.com>
2024-01-16 11:13:39 +0530

    changes

--- lib/shared/pages/patient_privacy_policy_page.dart
+++ lib/shared/pages/patient_privacy_policy_page.dart
@@ -1,5 +1,4 @@
 import 'package:eye_care_for_all/core/constants/app_color.dart';
-import 'package:eye_care_for_all/core/constants/app_images.dart';
 import 'package:eye_care_for_all/shared/extensions/widget_extension.dart';
 import 'package:eye_care_for_all/shared/theme/text_theme.dart';
 import 'package:eye_care_for_all/shared/widgets/custom_app_bar.dart';
