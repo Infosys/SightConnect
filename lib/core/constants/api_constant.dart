c31deafb79af7e44bd9da04ed9e0a2bec990f5ef
Monirul Islam <monirul.islam@infosys.com>
2024-02-01 11:58:40 +0530

    Update api_constant.dart

--- lib/core/constants/api_constant.dart
+++ lib/core/constants/api_constant.dart
@@ -2,7 +2,7 @@ class ApiConstant {
   ApiConstant._();
   static String baseUrl = 'https://eyecare4all-dev.infosysapps.com';
   static String patientRegistrationMiniappUrl =
-      '$baseUrl/patient-registration/';
+      '$baseUrl/patient-registration-dev/';
   static String playStoreUrl = 'https://healthconnecttech.org/install';
       // 'https://play.google.com/store/apps/details?id=com.healthconnect.sightconnect';
 }
