54d8788378a6474386b8cf817b3fbed0adec736d
mehul.mantoo <mehul.mantoo@infosys.com>
2024-02-19 12:45:36 +0530

    add pop scope and fix loading issue in camera screens

--- lib/core/services/permission_service.dart
+++ lib/core/services/permission_service.dart
@@ -1,4 +1,3 @@
-import 'package:eye_care_for_all/main.dart';
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 
@@ -6,65 +5,50 @@ class CameraPermissionService {
   static Future<bool> checkPermissions(
     BuildContext context,
   ) async {
-    logger.d(
-        'VisualAcuityFaceDistancePage: Check Permissions CameraPermissionService called');
-    final isGranted = await isCameraPermissionGranted();
+    PermissionStatus cameraStatus = await Permission.camera.status;
+    PermissionStatus audioStatus = await Permission.microphone.status;
+    final isGranted = cameraStatus.isGranted && audioStatus.isGranted;
     if (!isGranted) {
-      logger.d(
-          'VisualAcuityFaceDistancePage: Camera permission not granted. Requesting permission');
       final cameraStatus = await Permission.camera.request();
       final audioStatus = await Permission.microphone.request();
       if (cameraStatus.isGranted && audioStatus.isGranted) {
-        logger.d('VisualAcuityFaceDistancePage: Camera permission granted.');
         return true;
       } else {
         if (context.mounted) {
-          logger.d(
-              'VisualAcuityFaceDistancePage: Camera permission not granted. Opening settings');
           await _showCameraSettingsDialog(context);
         }
-        logger
-            .d('VisualAcuityFaceDistancePage: Camera permission not granted.');
         if (context.mounted) return await checkPermissions(context);
       }
     }
     return isGranted;
   }
 
-  static Future<bool> isCameraPermissionGranted() async {
-    logger.d('VisualAcuityFaceDistancePage: isCameraPermissionGranted called');
-    PermissionStatus cameraStatus = await Permission.camera.status;
-    PermissionStatus audioStatus = await Permission.microphone.status;
-    return cameraStatus.isGranted && audioStatus.isGranted;
-  }
-
   static _showCameraSettingsDialog(BuildContext context) {
-    logger.d('VisualAcuityFaceDistancePage: _showCameraSettingsDialog called');
     return showDialog(
       context: context,
       barrierDismissible: false,
-      builder: (BuildContext context) => AlertDialog(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(8.0),
-        ),
-        title: const Text('Permissions required'),
-        content: const Text(
-            'Please enable camera and microphone permissions in the app settings.'),
-        actions: <Widget>[
-          TextButton(
-            child: const Text('OK'),
-            onPressed: () async {
-              logger.d(
-                  "VisualAcuityFaceDistancePage: _showCameraSettingsDialog: OK button pressed");
-              await openAppSettings();
-              await Future.delayed(const Duration(seconds: 1));
-              if (context.mounted) {
-                Navigator.of(context).pop();
-                await checkPermissions(context);
-              }
-            },
+      builder: (BuildContext context) => PopScope(
+        canPop: false,
+        child: AlertDialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8.0),
           ),
-        ],
+          title: const Text('Permissions required'),
+          content: const Text(
+              'Please enable camera and microphone permissions in the app settings.'),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () async {
+                await openAppSettings();
+                await Future.delayed(const Duration(seconds: 1));
+                if (context.mounted) {
+                  Navigator.of(context).pop();
+                }
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
