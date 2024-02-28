5d0ff46c355bb6bc52a824ad1dc52d0849f83656
Abhishek Singh(abhishek.singh169) <abhishek.singh169@infosys.com>
2024-02-28 13:26:43 +0530

    Add language selection functionality to profile pages

--- lib/features/vision_technician/vision_technician_profile/presentation/pages/vt_profile_page.dart
+++ lib/features/vision_technician/vision_technician_profile/presentation/pages/vt_profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:eye_care_for_all/core/constants/app_size.dart';
+import 'package:eye_care_for_all/core/providers/global_language_provider.dart';
 import 'package:eye_care_for_all/core/providers/global_vt_provider.dart';
 import 'package:eye_care_for_all/features/common_features/initialization/pages/login_page.dart';
 import 'package:eye_care_for_all/features/common_features/initialization/providers/initilization_provider.dart';
@@ -7,7 +8,9 @@ import 'package:eye_care_for_all/features/vision_technician/vision_technician_pr
 import 'package:eye_care_for_all/features/vision_technician/vision_technician_profile/presentation/widgets/vt_profile_personal_details_card.dart';
 import 'package:eye_care_for_all/shared/extensions/widget_extension.dart';
 import 'package:eye_care_for_all/shared/widgets/custom_app_bar.dart';
+import 'package:eye_care_for_all/shared/widgets/translation_pop_up.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -25,6 +28,33 @@ class VTProfilePage extends ConsumerWidget {
           textAlign: TextAlign.left,
         ),
         actions: [
+          IconButton(
+            onPressed: () {
+              final currentLocaleCode =
+                  ref.read(globalLanguageProvider).currentLocale;
+              showModalBottomSheet(
+                isScrollControlled: true,
+                enableDrag: false,
+                isDismissible: false,
+                backgroundColor: Colors.transparent,
+                context: context,
+                builder: (context) => Container(
+                  height: MediaQuery.of(context).size.height,
+                  color: Colors.white,
+                  child: TranslationPopUp(
+                    locale: currentLocaleCode,
+                  ),
+                ),
+              );
+            },
+            icon: SvgPicture.asset(
+              "assets/drawer_icons/language.svg",
+              colorFilter: ColorFilter.mode(
+                Theme.of(context).iconTheme.color!,
+                BlendMode.srcIn,
+              ),
+            ),
+          ),
           IconButton(
             onPressed: () {
               final navigator = Navigator.of(context);
