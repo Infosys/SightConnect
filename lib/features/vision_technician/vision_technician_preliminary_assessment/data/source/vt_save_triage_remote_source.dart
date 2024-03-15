7b8cfbc5dfc7b7bb2f446fe2659cc19c235d85f7
Abhishek Singh(abhishek.singh169) <abhishek.singh169@infosys.com>
2024-03-15 13:01:08 +0530

    Fix exception handling and import statements

--- lib/features/vision_technician/vision_technician_preliminary_assessment/data/source/vt_save_triage_remote_source.dart
+++ lib/features/vision_technician/vision_technician_preliminary_assessment/data/source/vt_save_triage_remote_source.dart
@@ -2,6 +2,8 @@ import 'package:dio/dio.dart';
 import 'package:eye_care_for_all/features/common_features/triage/domain/models/triage_post_model.dart';
 import 'package:eye_care_for_all/main.dart';
 
+import '../../../../../core/services/exceptions.dart';
+
 abstract class VTSaveTriageRemoteSource {
   Future<TriagePostModel> saveTriage(TriagePostModel triagePostModel);
 }
@@ -10,18 +12,16 @@ class VTSaveTriageRemoteSourceImpl implements VTSaveTriageRemoteSource {
   final Dio dio;
 
   VTSaveTriageRemoteSourceImpl(this.dio);
-  
-
 
   @override
   Future<TriagePostModel> saveTriage(TriagePostModel triagePostModel) async {
-
     try {
       String endPoint = "/services/triage/api/triage-report";
       var response = await dio.post(endPoint, data: triagePostModel.toJson());
 
       return TriagePostModel.fromJson(response.data);
     } on DioException catch (e) {
+      DioErrorHandler.handleDioError(e);
       if (e.response != null) {
         logger.d("Error response: ${e.response.toString()}");
       }
@@ -30,6 +30,5 @@ class VTSaveTriageRemoteSourceImpl implements VTSaveTriageRemoteSource {
     }
 
     return const TriagePostModel();
-
   }
 }
