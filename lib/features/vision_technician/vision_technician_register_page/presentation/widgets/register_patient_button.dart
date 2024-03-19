8fdc1d030d1fe7622a60b53d5d0909d94388caf9
Abhishek Singh(abhishek.singh169) <abhishek.singh169@infosys.com>
2024-03-19 12:13:56 +0530

    Add vision technician register provider and invalidate vtRegisterProvider

--- lib/features/vision_technician/vision_technician_register_page/presentation/widgets/register_patient_button.dart
+++ lib/features/vision_technician/vision_technician_register_page/presentation/widgets/register_patient_button.dart
@@ -1,15 +1,17 @@
 import 'package:eye_care_for_all/core/constants/app_color.dart';
 import 'package:eye_care_for_all/features/common_features/initialization/pages/patient_registeration_miniapp_page.dart';
+import 'package:eye_care_for_all/features/vision_technician/vision_technician_register_page/presentation/providers/vision_technician_register_provider.dart';
 import 'package:eye_care_for_all/shared/extensions/widget_extension.dart';
 import 'package:eye_care_for_all/shared/theme/text_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_miniapp_web_runner/data/model/miniapp_injection_model.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class RegisterPatientButton extends StatelessWidget {
+class RegisterPatientButton extends ConsumerWidget {
   const RegisterPatientButton({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final loc = context.loc!;
 
     return OutlinedButton.icon(
@@ -22,7 +24,7 @@ class RegisterPatientButton extends StatelessWidget {
               displayName: loc.vtRegisterPatient,
             ),
           ),
-        );
+        ).then((value) => ref.invalidate(vtRegisterProvider));
       },
       icon: const Icon(Icons.add),
       label: Text(
