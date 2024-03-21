009dbb4315779719e17cc9ca141ad82c6c1f9216
Abhishek Singh(abhishek.singh169) <abhishek.singh169@infosys.com>
2024-03-21 12:36:21 +0530

    added env check on miniapp url

--- lib/core/constants/api_constant.dart
+++ lib/core/constants/api_constant.dart
@@ -4,26 +4,30 @@ class ApiConstant {
   ApiConstant._();
 
   static AppEnvironment appEnvironment = AppEnvironment.development;
+  static String baseUrl = Env.baseUrl;
+  static String patientRegistrationMiniappUrl =
+      '${baseUrl}/patient-registration/';
 
   static void setupEnv(AppEnvironment env) {
     appEnvironment = env;
     switch (env) {
-      case AppEnvironment.development:
-        baseUrl = Env.baseDevUrl;
-        break;
-      case AppEnvironment.staging:
-        baseUrl = Env.baseUrl;
-        break;
       case AppEnvironment.production:
         baseUrl = Env.baseUrl;
+        patientRegistrationMiniappUrl = '${baseUrl}/patient-registration/';
+        break;
+      case AppEnvironment.staging:
+        patientRegistrationMiniappUrl = '${baseUrl}/patient-registration/';
+        baseUrl = Env.baseUrl;
+        break;
+      case AppEnvironment.development:
+        patientRegistrationMiniappUrl =
+            '${Env.baseDevUrl}/patient-registration-dev/';
+        baseUrl = Env.baseDevUrl;
         break;
     }
   }
 
-  static String baseUrl = Env.baseUrl;
-  static String patientRegistrationMiniappUrl =
-      '${baseUrl}/patient-registration/';
-  static String playStoreUrl = '${baseUrl}/install';
+  // static String playStoreUrl = '${baseUrl}/install';
   static String chatbotUrl = "http://4.240.71.156:8000/api/chat/";
   static String webSocketEua =
       "ws://eyecare4all-dev.infosysapps.com/services/eua-service/api/v1/euaService/ws-client";
