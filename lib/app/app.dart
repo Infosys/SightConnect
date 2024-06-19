8f48315203c93b00b7ae11822e488930906a9085
Abhishek Singh(abhishek.singh169) <abhishek.singh169@infosys.com>
2024-06-19 10:49:50 +0530

    feat: Add Eye Bank Dashboard page

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -2,6 +2,7 @@ import 'package:eye_care_for_all/core/providers/global_language_provider.dart';
 import 'package:eye_care_for_all/core/providers/global_provider.dart';
 import 'package:eye_care_for_all/core/services/app_info_service.dart';
 import 'package:eye_care_for_all/core/services/persistent_auth_service.dart';
+import 'package:eye_care_for_all/features/common_features/dashboard/eye_bank_dashboard_page.dart';
 import 'package:eye_care_for_all/features/common_features/initialization/pages/initialization_page.dart';
 import 'package:eye_care_for_all/features/common_features/initialization/pages/login_page.dart';
 import 'package:eye_care_for_all/main.dart';
@@ -49,37 +50,36 @@ class MyApp extends ConsumerWidget {
           return const SecurePage();
         } else {
           return MediaQuery(
-              data: mediaQueryData.copyWith(
-                textScaler: Responsive.isMobile(context)
-                    ? TextScaler.linear(
-                        ref
-                            .watch(globalTextScaleFactorProvider)
-                            .textScaleFactor,
-                      )
-                    : const TextScaler.linear(1.3),
-              ),
-              child: AppFeedbackBetter(
-                child: MaterialApp(
-                  title: AppInfoService.appName,
-                  locale: ref.watch(globalLanguageProvider).currentLocale,
-                  localizationsDelegates: const [
-                    AppLocalizations.delegate,
-                    GlobalMaterialLocalizations.delegate,
-                    GlobalWidgetsLocalizations.delegate,
-                    GlobalCupertinoLocalizations.delegate,
-                  ],
-                  navigatorObservers: [matomoObserver],
-                  supportedLocales: appLocales.map((e) => Locale(e.locale)),
-                  debugShowCheckedModeBanner: false,
-                  themeMode: ref.watch(themeProvider),
-                  theme: ref.watch(themeProvider) == ThemeMode.light
-                      ? AppTheme.getLightTheme(context)
-                      : AppTheme.getDarkTheme(context),
-                  routes: AppRouter.routes,
+            data: mediaQueryData.copyWith(
+              textScaler: Responsive.isMobile(context)
+                  ? TextScaler.linear(
+                      ref.watch(globalTextScaleFactorProvider).textScaleFactor,
+                    )
+                  : const TextScaler.linear(1.3),
+            ),
+            child: AppFeedbackBetter(
+              child: MaterialApp(
+                title: AppInfoService.appName,
+                locale: ref.watch(globalLanguageProvider).currentLocale,
+                localizationsDelegates: const [
+                  AppLocalizations.delegate,
+                  GlobalMaterialLocalizations.delegate,
+                  GlobalWidgetsLocalizations.delegate,
+                  GlobalCupertinoLocalizations.delegate,
+                ],
+                navigatorObservers: [matomoObserver],
+                supportedLocales: appLocales.map((e) => Locale(e.locale)),
+                debugShowCheckedModeBanner: false,
+                themeMode: ref.watch(themeProvider),
+                theme: ref.watch(themeProvider) == ThemeMode.light
+                    ? AppTheme.getLightTheme(context)
+                    : AppTheme.getDarkTheme(context),
+                routes: AppRouter.routes,
 
-                initialRoute: initialRoute,
-                navigatorKey: AppRouter.navigatorKey,
-                onUnknownRoute: AppRouter.onUnknownRoute,
+                // initialRoute: initialRoute,
+                // navigatorKey: AppRouter.navigatorKey,
+                // onUnknownRoute: AppRouter.onUnknownRoute,
+                home: const EyeBankDashboardPage(),
                 // builder: (context, child) {
                 //   return ref.watch(internetProvider).maybeWhen(
                 //         data: (value) {
