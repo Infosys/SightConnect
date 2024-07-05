c4a9abb94ea513a7755ade8b5e78cc1589c6db9f
sarthak <sarthak.paranjpe@infosys.com>
2024-07-05 11:04:09 +0530

    Refactor DioTokenInterceptor to save tokens in PersistentAuthStateService

---
+++ lib/core/services/token_refresh_service.dart
@@ -0,0 +1,32 @@
+import 'dart:developer';
+
+import 'package:eye_care_for_all/core/services/persistent_auth_service.dart';
+import 'package:eye_care_for_all/features/common_features/initialization/providers/initilization_provider.dart';
+import 'package:eye_care_for_all/main.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+var dioRefreshTokenProvider = FutureProvider((ref) async {
+  Future<void> refreshToken() async {
+    final refreshToken = PersistentAuthStateService.authState.refreshToken;
+    final tokens = await ref
+        .read(initializationProvider)
+        .refreshTokens(refreshToken: refreshToken!);
+
+    log("newAccessToken: ${tokens!.accessToken}");
+    log("newRefreshToken: ${tokens.refreshToken}");
+
+    logger.d({
+      "newAccessToken": tokens!.accessToken,
+      "newRefreshToken": tokens.refreshToken,
+    });
+    // update the tokens in the persistent storage
+    await PersistentAuthStateService.authState
+        .updateAccessToken(accessToken: tokens.accessToken);
+    await PersistentAuthStateService.authState
+        .updateRefreshToken(refreshToken: tokens.refreshToken);
+    await PersistentAuthStateService.authState.saveTokens(
+        accessToken: tokens.accessToken, refreshToken: tokens.refreshToken);
+  }
+
+   await refreshToken();
+});
--- lib/features/vision_guardian/vision_guardian_add_event/presentation/pages/vg_add_event_details_page.dart
+++ lib/features/vision_guardian/vision_guardian_add_event/presentation/pages/vg_add_event_details_page.dart
@@ -1,25 +1,53 @@
 import 'package:eye_care_for_all/core/constants/app_color.dart';
 import 'package:eye_care_for_all/core/constants/app_size.dart';
+import 'package:eye_care_for_all/core/services/geocoding_service.dart';
+import 'package:eye_care_for_all/core/services/location_service.dart';
 import 'package:eye_care_for_all/features/vision_guardian/vision_guardian_add_event/presentation/providers/vg_add_event_details_provider.dart';
 import 'package:eye_care_for_all/features/vision_guardian/vision_guardian_add_event/presentation/widgets/vg_event_add_event_photo.dart';
 import 'package:eye_care_for_all/features/vision_guardian/vision_guardian_add_event/presentation/widgets/vg_event_address_card.dart';
-import 'package:eye_care_for_all/features/vision_guardian/vision_guardian_add_event/presentation/widgets/vg_event_datetime_card.dart';
 import 'package:eye_care_for_all/features/vision_guardian/vision_guardian_add_event/presentation/widgets/vg_event_bottom_navigation.dart';
+import 'package:eye_care_for_all/features/vision_guardian/vision_guardian_add_event/presentation/widgets/vg_event_datetime_card.dart';
 import 'package:eye_care_for_all/features/vision_guardian/vision_guardian_add_event/presentation/widgets/vg_form_helper_widgets.dart';
+import 'package:eye_care_for_all/main.dart';
 import 'package:eye_care_for_all/shared/extensions/widget_extension.dart';
 import 'package:eye_care_for_all/shared/regex/regex.dart';
 import 'package:eye_care_for_all/shared/theme/app_shadow.dart';
 import 'package:eye_care_for_all/shared/widgets/custom_app_bar.dart';
 import 'package:eye_care_for_all/shared/widgets/loading_overlay.dart';
 import 'package:flutter/material.dart';
-
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class VGAddEventDetailsPage extends ConsumerWidget {
+class VGAddEventDetailsPage extends ConsumerStatefulWidget {
   const VGAddEventDetailsPage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<VGAddEventDetailsPage> createState() =>
+      _VGAddEventDetailsPageState();
+}
+
+class _VGAddEventDetailsPageState extends ConsumerState<VGAddEventDetailsPage>
+    with WidgetsBindingObserver {
+   String? pincode;
+
+  @override
+  void initState() {
+    super.initState();
+    getPinode();
+  }
+
+  void getPinode() async {
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      final locationData = await LocationService.getLocationWithPermissions();
+      if (locationData != null) {
+        pincode = await GeocodingService.getPincodeFromLocation();
+        logger.f("Pincode : $pincode");
+      }
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     var data = ref.watch(addEventDetailsProvider);
     var formKey = data.formKey;
     final loc = context.loc!;
@@ -90,7 +118,7 @@ class VGAddEventDetailsPage extends ConsumerWidget {
                   const SizedBox(height: AppSize.km),
                   const VisionGuardianEventDateTimeCard(),
                   const SizedBox(height: AppSize.km),
-                  const VisionGuardianEventAddressCard(),
+                  VisionGuardianEventAddressCard( pincode: pincode),
                   const SizedBox(height: AppSize.km),
                 ],
               ),
