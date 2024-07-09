3a345b3a1574eeea94b9980c89b5a91a25fc232a
Abhishek Singh(abhishek.singh169) <abhishek.singh169@infosys.com>
2024-07-09 12:24:44 +0530

    chore: Update dynamic_form package to support image type in test_form.json

---
+++ packages/dynamic_form/lib/widgets/form_switch.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:form_builder_validators/form_builder_validators.dart';
+
+class FormSwitch extends HookWidget {
+  const FormSwitch({
+    super.key,
+    required this.map,
+    required this.onChanged,
+  });
+
+  final Map<String, dynamic> map;
+  final void Function(bool) onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    final selected = useState(map['initialValue']);
+    return FormBuilderSwitch(
+      name: map['name'],
+      initialValue: selected.value,
+      validator:
+          FormBuilderValidators.required(errorText: 'This field is required'),
+      title: Text(map['title']),
+      onChanged: (value) {
+        selected.value = value;
+        onChanged(value ?? false);
+      },
+    );
+  }
+}
