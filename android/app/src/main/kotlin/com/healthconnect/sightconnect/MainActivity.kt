0a22ed007e13e4ce1aa87aa585fa7365ed4d68ae
Abhishek Singh(abhishek.singh169) <abhishek.singh169@infosys.com>
2024-07-18 22:54:31 +0530

    apm

--- android/app/src/main/kotlin/com/healthconnect/sightconnect/MainActivity.kt
+++ android/app/src/main/kotlin/com/healthconnect/sightconnect/MainActivity.kt
@@ -4,12 +4,15 @@ import android.content.Context
 import android.hardware.camera2.CameraAccessException
 import android.hardware.camera2.CameraCharacteristics
 import android.hardware.camera2.CameraManager
+import android.os.Bundle
 import androidx.annotation.NonNull
+import co.elastic.apm.android.sdk.ElasticApmAgent
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 class MainActivity: FlutterActivity() {
     private val CHANNEL = "com.healthconnect.sightconnect/camera"
+
 
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
