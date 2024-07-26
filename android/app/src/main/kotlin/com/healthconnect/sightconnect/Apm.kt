43dca0ae1071e34b978c67769246515bf78f882d
Abhishek Singh(abhishek.singh169) <abhishek.singh169@infosys.com>
2024-07-26 14:05:57 +0530

    removed apm

--- android/app/src/main/kotlin/com/healthconnect/sightconnect/Apm.kt
+++ android/app/src/main/kotlin/com/healthconnect/sightconnect/Apm.kt
@@ -1,13 +1,13 @@
-package com.healthconnect.sightconnect
-import co.elastic.apm.android.sdk.ElasticApmAgent
+// package com.healthconnect.sightconnect
+// import co.elastic.apm.android.sdk.ElasticApmAgent
 
 
-class Apm : android.app.Application() {
+// class Apm : android.app.Application() {
 
-    override fun onCreate() {
-        super.onCreate()
-        println("Initializing Elastic APM agent...")
-        ElasticApmAgent.initialize(this)
+//     override fun onCreate() {
+//         super.onCreate()
+//         println("Initializing Elastic APM agent...")
+//         ElasticApmAgent.initialize(this)
 
-    }
-}
+//     }
+// }
