ef5cad53d2d9da19661a37583d91255603520d3c
sarthak <sarthak.paranjpe@infosys.com>
2024-07-29 13:53:22 +0530

    feat: Enable adding events for Vision Guardians

--- lib/features/vision_guardian/vision_guardian_add_event/presentation/pages/vg_create_event_page.dart
+++ lib/features/vision_guardian/vision_guardian_add_event/presentation/pages/vg_create_event_page.dart
@@ -1,7 +1,10 @@
+import 'package:eye_care_for_all/core/services/persistent_auth_service.dart';
+import 'package:eye_care_for_all/features/vision_guardian/vision_guardian_add_event/presentation/pages/vg_add_event_details_page.dart';
 import 'package:eye_care_for_all/features/vision_guardian/vision_guardian_add_event/presentation/providers/vg_add_event_details_provider.dart';
 import 'package:eye_care_for_all/features/vision_guardian/vision_guardian_add_event/presentation/widgets/vg_event_list_details.dart';
 import 'package:eye_care_for_all/features/vision_guardian/vision_guardian_add_event/presentation/widgets/vg_event_search.dart';
 import 'package:eye_care_for_all/shared/extensions/widget_extension.dart';
+import 'package:eye_care_for_all/shared/theme/text_theme.dart';
 import 'package:eye_care_for_all/shared/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -54,60 +57,60 @@ class VisionGuardianEventPage extends HookConsumerWidget {
           ),
         ),
       ),
-      // floatingActionButton:
-
-      // InkWell(
-      //   onTap: () {
-      //     ref.read(addEventDetailsProvider).setIsLoading();
-      //     Navigator.push(
-      //       context,
-      //       MaterialPageRoute(
-      //         builder: (context) => const VGAddEventDetailsPage(),
-      //       ),
-      //     );
-      //   },
-      //   child: Container(
-      //     width: AppSize.width(context) * 0.35,
-      //     height: AppSize.height(context) * 0.06,
-      //     padding: const EdgeInsets.only(bottom: AppSize.ks - 1),
-      //     margin: const EdgeInsets.only(bottom: AppSize.kl),
-      //     decoration: BoxDecoration(
-      //       borderRadius: BorderRadius.circular(10),
-      //       color: AppColor.yellow,
-      //       boxShadow: const <BoxShadow>[
-      //         BoxShadow(
-      //           color: AppColor.grey,
-      //           blurRadius: 8,
-      //           offset: Offset(0, 4),
-      //         ),
-      //       ],
-      //     ),
-      //     child: Center(
-      //       child: RichText(
-      //         text: TextSpan(
-      //           children: <TextSpan>[
-      //             TextSpan(
-      //               text: '+ ',
-      //               style: applyRobotoFont(
-      //                   fontSize: 24,
-      //                   color: AppColor.black,
-      //                   fontWeight: FontWeight.w400),
-      //             ),
-      //             TextSpan(
-      //               text: loc.vgAddEvent,
-      //               style: applyRobotoFont(
-      //                 fontSize: 14,
-      //                 fontWeight: FontWeight.w500,
-      //                 color: AppColor.black,
-      //               ),
-      //             ),
-      //           ],
-      //         ),
-      //       ),
-      //     ),
-      //   ),
-      // ),
-
+      floatingActionButton: PersistentAuthStateService.authState.activeRole ==
+              "ROLE_VISION_GUARDIAN"
+          ? InkWell(
+              onTap: () {
+                ref.read(addEventDetailsProvider).setIsLoading();
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const VGAddEventDetailsPage(),
+                  ),
+                );
+              },
+              child: Container(
+                width: AppSize.width(context) * 0.35,
+                height: AppSize.height(context) * 0.06,
+                padding: const EdgeInsets.only(bottom: AppSize.ks - 1),
+                margin: const EdgeInsets.only(bottom: AppSize.kl),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: AppColor.yellow,
+                  boxShadow: const <BoxShadow>[
+                    BoxShadow(
+                      color: AppColor.grey,
+                      blurRadius: 8,
+                      offset: Offset(0, 4),
+                    ),
+                  ],
+                ),
+                child: Center(
+                  child: RichText(
+                    text: TextSpan(
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: '+ ',
+                          style: applyRobotoFont(
+                              fontSize: 24,
+                              color: AppColor.black,
+                              fontWeight: FontWeight.w400),
+                        ),
+                        TextSpan(
+                          text: loc.vgAddEvent,
+                          style: applyRobotoFont(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                            color: AppColor.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            )
+          : null,
       body: SingleChildScrollView(
         controller: ref.watch(addEventDetailsProvider).eventListController,
         child: Consumer(
