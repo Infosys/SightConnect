41b41272d6039be1f0ba2dd9cb9f89183ef17bc9
Avendra <avendrasingh.gaur@infosys.com>
2024-08-02 12:01:34 +0530

    refactor: Adding flow for screening of a case

--- lib/apps/eyebank/features/screening/presentation/pages/eb_screening_page.dart
+++ lib/apps/eyebank/features/screening/presentation/pages/eb_screening_page.dart
@@ -1,8 +1,9 @@
 import 'package:dynamic_form/pages/dynamic_form_page.dart';
-import 'package:eye_care_for_all/main.dart';
+import 'package:eye_care_for_all/apps/eyebank/features/screening/presentation/provider/eb_screening_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class EbScreeningPage extends StatelessWidget {
+class EbScreeningPage extends ConsumerWidget {
   final String title;
   final String caseID;
 
@@ -13,17 +14,21 @@ class EbScreeningPage extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Screening CaseID : $caseID'),
-      ),
-      body: DynamicFormPage(
-        json: '',
-        onSubmit: (value) {
-          logger.i(value);
-        },
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Screening CaseID : $caseID'),
+        ),
+        body: ref.watch(ebScreeningProvider).when(
+              data: (json) {
+                return DynamicFormPage(json: json);
+              },
+              loading: () => const Center(
+                child: CircularProgressIndicator(),
+              ),
+              error: (error, stackTrace) => Center(
+                child: Text('Error: $error'),
+              ),
+            ));
   }
 }
--- lib/apps/eyebank/features/screening/presentation/provider/eb_screening_provider.dart
+++ lib/apps/eyebank/features/screening/presentation/provider/eb_screening_provider.dart
@@ -2,10 +2,10 @@ import 'package:eye_care_for_all/main.dart';
 import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-final ebCaseCreationProvider = FutureProvider<String>((ref) async {
+final ebScreeningProvider = FutureProvider<String>((ref) async {
   try {
     return await rootBundle.loadString(
-      'assets/local_json/case_register.json',
+      'assets/local_json/screening_form.json',
     );
   } catch (e) {
     logger.f('Error Converting json: $e');
