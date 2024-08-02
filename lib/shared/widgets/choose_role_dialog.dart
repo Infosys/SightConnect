1233873d47e8fa51d9b83016aebd53da0f9f5cc1
sarthak <sarthak.paranjpe@infosys.com>
2024-08-02 12:18:49 +0530

    refactor: Add "EYE BANK" role to ChooseRoleDialog

--- lib/shared/widgets/choose_role_dialog.dart
+++ lib/shared/widgets/choose_role_dialog.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:eye_care_for_all/shared/constants/app_color.dart';
 import 'package:eye_care_for_all/shared/extensions/widget_extension.dart';
+import 'package:eye_care_for_all/shared/services/persistent_auth_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
@@ -157,6 +158,7 @@ class ChooseRoleDialog extends HookWidget {
       Role.ROLE_PATIENT => loc.rolePatient,
       Role.ROLE_VISION_GUARDIAN => loc.roleVisionGuardian,
       Role.ROLE_VOLUNTEER => "VOLUNTEER",
+      Role.ROLE_EYEBANK => "EYE BANK",
     };
   }
 }
