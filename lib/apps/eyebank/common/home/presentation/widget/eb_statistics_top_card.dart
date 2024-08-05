86d4938f5dfceb85cf6f9b69ed5a3e679c5857aa
Abhishek Singh(abhishek.singh169) <abhishek.singh169@infosys.com>
2024-08-05 14:40:55 +0530

    refactor: Update padding and layout in EbStatisticsTopCard widget

--- lib/apps/eyebank/common/home/presentation/widget/eb_statistics_top_card.dart
+++ lib/apps/eyebank/common/home/presentation/widget/eb_statistics_top_card.dart
@@ -10,7 +10,7 @@ class EbStatisticsTopCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(AppSize.kl),
+      padding: const EdgeInsets.all(AppSize.km),
       width: AppSize.width(context) - AppSize.ks * 2,
       decoration: BoxDecoration(
         color: AppColor.pureBlue,
@@ -27,15 +27,29 @@ class EbStatisticsTopCard extends StatelessWidget {
             ),
           ),
           const SizedBox(height: AppSize.km),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              _buildColumn('1,58,790', 'OverAll', context, isSelected: true),
-              _buildColumn('80/200', 'Today', context),
-              _buildColumn('1400', 'Today', context),
-              _buildColumn('6200', 'Today', context),
-            ],
+          Scrollbar(
+            interactive: true,
+            radius: const Radius.circular(10),
+            thumbVisibility: true,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: SizedBox(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      _buildColumn('1,58,790', 'Overall', context,
+                          isSelected: true),
+                      _buildColumn('80/200', 'Today', context),
+                      _buildColumn('1400', 'Today', context),
+                      _buildColumn('6200', 'Today', context),
+                    ],
+                  ),
+                ),
+              ),
+            ),
           ),
         ],
       ),
@@ -44,39 +58,37 @@ class EbStatisticsTopCard extends StatelessWidget {
 
   Widget _buildColumn(String number, String? text, BuildContext context,
       {bool isSelected = false}) {
-    return Flexible(
-      child: Container(
-        padding: const EdgeInsets.all(AppSize.km),
-        decoration: BoxDecoration(
-            color: isSelected ? AppColor.primary : AppColor.pureBlue,
-            borderRadius: BorderRadius.circular(AppSize.ks)),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            if (text != null)
-              AutoSizeText(
-                text,
-                style: applyRobotoFont(
-                  fontSize: 14,
-                  color: isSelected ? AppColor.white : AppColor.grey,
-                  fontWeight: FontWeight.w500,
-                ),
-                minFontSize: 10,
-                maxFontSize: 14,
-              ),
+    return Container(
+      padding: const EdgeInsets.all(AppSize.km),
+      decoration: BoxDecoration(
+        color: isSelected ? AppColor.primary : AppColor.pureBlue,
+        borderRadius: BorderRadius.circular(AppSize.ks),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          if (text != null)
             AutoSizeText(
-              number,
-              style: applyFiraSansFont(
-                fontSize: 28,
-                color: isSelected ? AppColor.white : AppColor.primary,
+              text,
+              style: applyRobotoFont(
+                fontSize: 14,
+                color: isSelected ? AppColor.white : AppColor.grey,
                 fontWeight: FontWeight.w500,
               ),
-              minFontSize: 20,
-              maxFontSize: 28,
-              maxLines: 1,
+              minFontSize: 10,
+              maxFontSize: 14,
             ),
-          ],
-        ),
+          AutoSizeText(
+            number,
+            style: applyFiraSansFont(
+              fontSize: 28,
+              color: isSelected ? AppColor.white : AppColor.primary,
+              fontWeight: FontWeight.w500,
+            ),
+            minFontSize: 20,
+            maxFontSize: 28,
+          ),
+        ],
       ),
     );
   }
