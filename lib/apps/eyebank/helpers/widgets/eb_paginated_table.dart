59cf14fff3c42a4ccd032689e88a6253be9ba25f
mehul.mantoo <mehul.mantoo@infosys.com>
2024-08-05 16:55:23 +0530

    improve search ui and extract filter and search as different widgets

--- lib/apps/eyebank/helpers/widgets/eb_paginated_table.dart
+++ lib/apps/eyebank/helpers/widgets/eb_paginated_table.dart
@@ -58,7 +58,11 @@ class EBPaginatedTableState<T> extends State<EBPaginatedTable<T>> {
       isLoading = true;
     });
     data = await widget.fetchData(
-        currentPage, rowsPerPage, searchQuery, selectedFilter);
+      currentPage,
+      rowsPerPage,
+      searchQuery,
+      selectedFilter,
+    );
     setState(() {
       isLoading = false;
     });
@@ -68,49 +72,8 @@ class EBPaginatedTableState<T> extends State<EBPaginatedTable<T>> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: TextField(
-            decoration: const InputDecoration(
-              labelText: 'Search',
-              border: OutlineInputBorder(),
-            ),
-            onChanged: _onSearchChanged,
-          ),
-        ),
-        SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Wrap(
-            spacing: 8,
-            children: widget.filterOptions.map((filter) {
-              return ChoiceChip(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 14),
-                label: Text(filter),
-                selected: selectedFilter == filter,
-                onSelected: (selected) {
-                  setState(() {
-                    selectedFilter = selected ? filter : null;
-                    currentPage = 0;
-                    _fetchData();
-                  });
-                },
-                selectedColor: selectedFilter == filter
-                    ? AppColor.primary
-                    : AppColor.lightGrey,
-                labelStyle: applyRobotoFont(
-                  color: selectedFilter == filter
-                      ? AppColor.white
-                      : AppColor.black,
-                  fontSize: 12,
-                ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
-                ),
-              );
-            }).toList(),
-          ),
-        ),
+        _buildSearchBar(context),
+        _buildFilterChips(context),
         Expanded(
           child: isLoading
               ? const Center(child: CircularProgressIndicator())
@@ -158,5 +121,79 @@ class EBPaginatedTableState<T> extends State<EBPaginatedTable<T>> {
   void dispose() {
     _debounce?.cancel();
     super.dispose();
+  }
+
+  Widget _buildFilterChips(context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Wrap(
+        spacing: 8,
+        children: widget.filterOptions.map((filter) {
+          return ChoiceChip(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 14),
+            label: Text(filter),
+            selected: selectedFilter == filter,
+            onSelected: (selected) {
+              setState(() {
+                selectedFilter = selected ? filter : null;
+                currentPage = 0;
+                _fetchData();
+              });
+            },
+            selectedColor: selectedFilter == filter
+                ? AppColor.primary
+                : AppColor.lightGrey,
+            labelStyle: applyRobotoFont(
+              color: selectedFilter == filter ? AppColor.white : AppColor.black,
+              fontSize: 12,
+            ),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+          );
+        }).toList(),
+      ),
+    );
+  }
+
+    Widget _buildSearchBar(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextField(
+        decoration: InputDecoration(
+          contentPadding: const EdgeInsets.symmetric(vertical: 22.0, horizontal: 20),
+          hintText: 'Search...',
+          prefixIcon: const Padding(
+            padding: EdgeInsets.only(left: 8, right: 8),
+            child: Icon(
+              Icons.search,
+              size: 28,
+            ),
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12.0),
+            borderSide: BorderSide(
+              color: Colors.grey.shade300,
+              width: 1.0,
+            ),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12.0),
+            borderSide: BorderSide(
+              color: Colors.grey.shade300,
+              width: 1.0,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12.0),
+            borderSide: const BorderSide(
+              color: AppColor.primary,
+              width: 1.0,
+            ),
+          ),
+        ),
+        onChanged: _onSearchChanged,
+      ),
+    );
   }
 }
