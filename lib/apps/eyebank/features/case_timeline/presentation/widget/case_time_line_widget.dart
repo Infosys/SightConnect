2636be9a64ab4fe9c84fe0bf6644b18351e89a01
Avendra <avendrasingh.gaur@infosys.com>
2024-08-06 13:18:02 +0530

    feat: Add CaseInfoTile widget for displaying case information

--- lib/apps/eyebank/features/case_timeline/presentation/widget/case_time_line_widget.dart
+++ lib/apps/eyebank/features/case_timeline/presentation/widget/case_time_line_widget.dart
@@ -1,8 +1,11 @@
+import 'package:eye_care_for_all/apps/eyebank/features/case_timeline/presentation/widget/case_info_tile.dart';
+import 'package:eye_care_for_all/shared/constants/app_color.dart';
+import 'package:eye_care_for_all/shared/theme/text_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:timelines/timelines.dart';
 
 class CaseTimeLineWidget extends StatelessWidget {
-  final List<Map<String, String>> caseTimeLine;
+  final dynamic caseTimeLine;
   const CaseTimeLineWidget({
     super.key,
     required this.caseTimeLine,
@@ -15,7 +18,7 @@ class CaseTimeLineWidget extends StatelessWidget {
       child: Timeline.tileBuilder(
         theme: TimelineThemeData(
           nodePosition: 0,
-          color: Colors.blue,
+          color: Colors.grey,
           indicatorTheme: const IndicatorThemeData(
             position: 0,
             size: 20.0,
@@ -24,60 +27,136 @@ class CaseTimeLineWidget extends StatelessWidget {
             thickness: 2.5,
           ),
         ),
-        builder: TimelineTileBuilder.fromStyle(
+        builder: TimelineTileBuilder.connected(
           contentsBuilder: (context, index) {
             final event = caseTimeLine[index];
             final status = event['status'];
             final isCompleted = status == 'COMPLETED';
-            return Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: Row(
-                    children: [
-                      Text(
-                        event['serviceRequestCode'] ?? 'Unknown',
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 14,
+            return Padding(
+              padding: const EdgeInsets.only(bottom: 32.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Row(
+                      children: [
+                        Text(
+                          event['serviceRequestCode'] ?? 'Unknown',
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14,
+                          ),
                         ),
-                      ),
-                      const Spacer(),
-                      Chip(
-                        label: Text(
-                          status ?? 'Unknown',
-                          style: TextStyle(
-                              color: isCompleted ? Colors.white : Colors.black,
-                              fontSize: 12),
+                        Visibility(
+                          visible: index == caseTimeLine.length - 1,
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 12.0),
+                            child: Text(
+                              'END',
+                              style: applyRobotoFont(
+                                fontSize: 12,
+                                color: AppColor.red,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
                         ),
-                        backgroundColor:
-                            isCompleted ? Colors.green : Colors.red,
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(24.0),
-                  child: Card(
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const SizedBox(height: 8),
-                          Text(event['description'] ?? 'No description'),
-                          const SizedBox(height: 8),
-                          Text(event['timestamp'] ?? 'No timestamp'),
-                        ],
-                      ),
+                      ],
                     ),
                   ),
-                ),
-              ],
+                  isCompleted
+                      ? Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Card(
+                            child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const SizedBox(height: 8),
+                                  CaseInfoTile(
+                                    title: 'Description',
+                                    value: event['description'],
+                                  ),
+                                  const SizedBox(height: 8),
+                                  CaseInfoTile(
+                                    title: 'Timestamp',
+                                    value: event['timestamp']?.split('T').first,
+                                  ),
+                                  const SizedBox(height: 8),
+                                  CaseInfoTile(
+                                    title: 'Status',
+                                    value: status,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        )
+                      : const SizedBox(),
+                ],
+              ),
             );
           },
-          indicatorStyle: IndicatorStyle.dot,
+          indicatorBuilder: (context, index) {
+            final event = caseTimeLine[index];
+            final status = event['status'];
+            final isCompleted = status == 'COMPLETED';
+            final isActive = status == 'ACTIVE';
+            final isPending = status == 'PENDING';
+
+            if (isCompleted) {
+              return const DotIndicator(
+                color: AppColor.green,
+                child: Icon(
+                  Icons.check,
+                  color: Colors.white,
+                  size: 12.0,
+                ),
+              );
+            } else if (isActive) {
+              return const DotIndicator(
+                color: AppColor.primary,
+                child: Icon(
+                  Icons.more_horiz,
+                  color: Colors.white,
+                  size: 12.0,
+                ),
+              );
+            } else if (isPending) {
+              return const DotIndicator(
+                color: AppColor.grey,
+                child: Icon(
+                  Icons.access_time,
+                  color: Colors.white,
+                  size: 12.0,
+                ),
+              );
+            }
+            return null;
+          },
+          connectorBuilder: (context, index, type) {
+            final event = caseTimeLine[index];
+            final status = event['status'];
+            final isCompleted = status == 'COMPLETED';
+            final isActive = status == 'ACTIVE';
+            final isPending = status == 'PENDING';
+            if (isCompleted) {
+              return const SolidLineConnector(color: AppColor.green);
+            } else if (isActive) {
+              return const SolidLineConnector(color: AppColor.primary);
+            } else if (isPending) {
+              return const SolidLineConnector(color: AppColor.grey);
+            }
+            return null;
+          },
+          firstConnectorBuilder: (context) {
+            return const DashedLineConnector(color: Colors.grey);
+          },
           itemCount: caseTimeLine.length,
+          indicatorPositionBuilder: (context, index) => 0,
+          nodePositionBuilder: (context, index) => 0,
         ),
       ),
     );
