487451dabbad7f50484719e1b4185d88677b976b
Abhishek Singh(abhishek.singh169) <abhishek.singh169@infosys.com>
2024-08-12 16:41:02 +0530

    refactor: Update repeated-panel layout in PageWidget
    This commit updates the layout of the repeated-panel in the PageWidget. It introduces a new feature that allows users to dynamically add repeated panels by clicking on the add button. The repeated panels are now displayed in a row with a grey background color. Each added panel contains a set of fields wrapped in an AppCard. This enhancement improves the user experience and provides a more intuitive way to interact with the form.

--- lib/apps/eyebank/features/organ_inventory/widgets/organ_request_widget.dart
+++ lib/apps/eyebank/features/organ_inventory/widgets/organ_request_widget.dart
@@ -1,5 +1,8 @@
+import 'package:eye_care_for_all/apps/eyebank/features/case_timeline/presentation/pages/eb_case_time_line_page.dart';
 import 'package:eye_care_for_all/apps/eyebank/features/organ_inventory/modals/organ_tissue_search_delegate.dart';
 import 'package:eye_care_for_all/main.dart';
+import 'package:eye_care_for_all/shared/constants/app_color.dart';
+import 'package:eye_care_for_all/shared/theme/text_theme.dart';
 import 'package:flutter/material.dart';
 
 class OrganRequestWidget extends StatefulWidget {
@@ -15,67 +18,71 @@ class _OrganRequestWidgetState extends State<OrganRequestWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const SizedBox(height: 8),
-        Wrap(
-          spacing: 8.0,
-          children: _filters.map((filter) {
-            return FilterChip(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8.0),
-              ),
-              iconTheme: const IconThemeData(color: Colors.white),
-              label: Text(filter),
-              selected: _selectedFilter == filter,
-              onSelected: (bool selected) {
-                setState(() {
-                  _selectedFilter = selected ? filter : 'All';
-                });
-              },
-            );
-          }).toList(),
-        ),
-        Expanded(
-          child: CustomScrollView(
-            slivers: [
-              SliverList(
-                delegate: SliverChildBuilderDelegate(
-                  (context, index) {
-                    // Filter logic based on _selectedFilter
-                    if (_selectedFilter != 'All' &&
-                        _selectedFilter != 'Pending') {
-                      return Container(); // Example filter logic
-                    }
-                    return Card(
-                      margin: const EdgeInsets.all(8.0),
-                      child: ListTile(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8.0),
-                        ),
-                        contentPadding: const EdgeInsets.all(10.0),
-                        title: Text('Cornea Request #$index'),
-                        subtitle: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Text('Doctor: Dr. Smith'),
-                            const SizedBox(height: 8),
-                            Text('Details about cornea request #$index'),
-                          ],
-                        ),
-                        trailing: IconButton(
-                          icon: const Icon(Icons.arrow_forward),
-                          onPressed: () {
-                            _showAssignmentFlow(context, index);
-                          },
-                        ),
-                      ),
-                    );
-                  },
-                  childCount: 20,
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          _buildFilterChips(),
+          Expanded(
+            child: _buildRequestList(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildFilterChips() {
+    return Wrap(
+      spacing: 8.0,
+      children: _filters.map((filter) {
+        return FilterChip(
+          backgroundColor: AppColor.lightGrey,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          label: Text(
+            filter,
+            style: applyFiraSansFont(
+              fontSize: 12.0,
+              color:
+                  _selectedFilter == filter ? AppColor.primary : Colors.black,
+            ),
+          ),
+          selected: _selectedFilter == filter,
+          onSelected: (bool selected) {
+            setState(() {
+              _selectedFilter = selected ? filter : 'All';
+            });
+          },
+        );
+      }).toList(),
+    );
+  }
+
+  Widget _buildRequestList() {
+    return CustomScrollView(
+      slivers: [
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              if (_selectedFilter != 'All' && _selectedFilter != 'Pending') {
+                return Container();
+              }
+              return InkWell(
+                onTap: () {
+                  _showAssignmentFlow(context, index);
+                },
+                child: const OrganRequestCard(
+                  doctorName: 'Dr. John Doe',
+                  priority: 'High',
+                  tissue: 'Cornea',
+                  eyeInvolved: 'Right Eye',
+                  tissueExpiryTime: '2 days 3 hours',
+                  transplantationTechnique: 'PKP, EK, ALK',
                 ),
-              ),
-            ],
+              );
+            },
+            childCount: 20,
           ),
         ),
       ],
@@ -93,5 +100,179 @@ class _OrganRequestWidgetState extends State<OrganRequestWidget> {
       // Handle the selected tissue assignment
       logger.f('Assigned $selectedTissue to request #$requestIndex');
     }
+  }
+}
+
+class OrganRequestCard extends StatelessWidget {
+  final String doctorName;
+  final String priority;
+  final String tissue;
+  final String eyeInvolved;
+  final String tissueExpiryTime;
+  final String transplantationTechnique;
+
+  const OrganRequestCard({
+    Key? key,
+    required this.doctorName,
+    required this.priority,
+    required this.tissue,
+    required this.eyeInvolved,
+    required this.tissueExpiryTime,
+    required this.transplantationTechnique,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      margin: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.1),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Expanded(
+                flex: 2,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const CircleAvatar(
+                      backgroundColor: AppColor.altGrey,
+                    ),
+                    const SizedBox(width: 8.0),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          doctorName,
+                          style: applyRobotoFont(
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        Text(
+                          "M.D. Ophthalmology",
+                          style: applyRobotoFont(
+                            fontSize: 12.0,
+                            color: AppColor.blackOpacity,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const Spacer(),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: Chip(
+                        padding: const EdgeInsets.all(2),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        label: Text(priority),
+                        backgroundColor: AppColor.mediumRed,
+                        labelStyle: applyFiraSansFont(
+                          fontSize: 12.0,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: ActionChip(
+                        onPressed: () {
+                          final navigator = Navigator.of(context);
+                          navigator.push(
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  const EbCaseTimeLinePage(caseID: "1234"),
+                            ),
+                          );
+                        },
+                        padding: const EdgeInsets.all(2),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        label: const Icon(
+                          Icons.timeline,
+                          color: Colors.white,
+                        ),
+                        backgroundColor: AppColor.mediumOrange,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 4.0),
+          Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: _buildDetailColumn('Tissue:', tissue),
+              ),
+              Expanded(
+                flex: 1,
+                child: _buildDetailColumn('Eye Involved:', eyeInvolved),
+              ),
+            ],
+          ),
+          const SizedBox(height: 8.0),
+          Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child:
+                    _buildDetailColumn('Tissue Expiry Time:', tissueExpiryTime),
+              ),
+              Expanded(
+                flex: 1,
+                child: _buildDetailColumn(
+                    'Transplantation Technique:', transplantationTechnique),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDetailColumn(String label, String value) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+          style: applyFiraSansFont(
+            fontSize: 12.0,
+            color: AppColor.grey,
+          ),
+        ),
+        const SizedBox(height: 2.0),
+        Text(
+          value,
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis,
+          style: applyRobotoFont(
+            fontSize: 12.0,
+            color: AppColor.black,
+          ),
+        ),
+      ],
+    );
   }
 }
