deed57da23a1b3b9f7f326f7521c8a850c444780
Abhishek Singh(abhishek.singh169) <abhishek.singh169@infosys.com>
2024-08-13 16:45:00 +0530

    refactor: Update repeated-panel layout in PageWidget

--- lib/apps/eyebank/features/organ_inventory/widgets/organ_inventory_overview.dart
+++ lib/apps/eyebank/features/organ_inventory/widgets/organ_inventory_overview.dart
@@ -1,3 +1,5 @@
+import 'package:eye_care_for_all/shared/constants/app_color.dart';
+import 'package:eye_care_for_all/shared/theme/text_theme.dart';
 import 'package:flutter/material.dart';
 
 class OrganInventoryOverview extends StatelessWidget {
@@ -5,38 +7,38 @@ class OrganInventoryOverview extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Example inventory stats data
     final inventoryStats = [
-      {'title': 'Total Corneas', 'count': 50, 'icon': Icons.visibility},
+      {'title': 'Total Corneas', 'count': 232, 'icon': Icons.visibility},
       {'title': 'Available Corneas', 'count': 30, 'icon': Icons.check_circle},
       {'title': 'Pending Requests', 'count': 10, 'icon': Icons.pending},
       {'title': 'Completed Requests', 'count': 10, 'icon': Icons.done_all},
     ];
 
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverPadding(
-            padding: const EdgeInsets.all(10.0),
-            sliver: SliverGrid(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: CustomScrollView(
+          slivers: [
+            SliverGrid(
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 2,
                 mainAxisSpacing: 10.0,
                 crossAxisSpacing: 10.0,
-                childAspectRatio: 0.8, // Adjusted aspect ratio
+                childAspectRatio: 1.1,
               ),
               delegate: SliverChildBuilderDelegate(
                 (context, index) {
                   final stat = inventoryStats[index];
                   return Container(
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15.0),
                       color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0),
                       boxShadow: [
                         BoxShadow(
-                          color: Colors.black26,
-                          blurRadius: 5.0,
-                          offset: Offset(2, 2),
+                          color: Colors.grey.withOpacity(0.1),
+                          spreadRadius: 2,
+                          blurRadius: 5,
+                          offset: const Offset(0, 3),
                         ),
                       ],
                     ),
@@ -45,30 +47,37 @@ class OrganInventoryOverview extends StatelessWidget {
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Icon(
-                            stat['icon'] as IconData,
-                            size: 40.0,
-                            color: Colors.black,
+                          Flexible(
+                            child: Text(
+                              (stat['count'] as int).toString(),
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 1,
+                              style: applyRobotoFont(
+                                fontSize: 40.0,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.black,
+                              ),
+                            ),
                           ),
-                          const SizedBox(height: 12.0),
+                          Text(
+                            " ^ 15%",
+                            textAlign: TextAlign.center,
+                            style: applyRobotoFont(
+                              fontSize: 12.0,
+                              color: AppColor.altGreen,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          const SizedBox(height: 16.0),
                           Text(
                             stat['title'] as String? ?? '',
                             textAlign: TextAlign.center,
-                            style: const TextStyle(
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black,
+                            style: applyRobotoFont(
+                              fontSize: 12.0,
+                              color: AppColor.grey,
                             ),
                           ),
                           const SizedBox(height: 8.0),
-                          Text(
-                            (stat['count'] as int).toString(),
-                            style: const TextStyle(
-                              fontSize: 24.0,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black,
-                            ),
-                          ),
                         ],
                       ),
                     ),
@@ -77,64 +86,67 @@ class OrganInventoryOverview extends StatelessWidget {
                 childCount: inventoryStats.length,
               ),
             ),
-          ),
-          SliverPadding(
-            padding: const EdgeInsets.all(10.0),
-            sliver: SliverList(
+            SliverList(
               delegate: SliverChildBuilderDelegate(
-                (context, index) => Card(
-                  margin: const EdgeInsets.all(8.0),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15.0),
+                (context, index) => Container(
+                  margin: const EdgeInsets.symmetric(vertical: 8),
+                  padding: const EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.1),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                        offset: const Offset(0, 3),
+                      ),
+                    ],
                   ),
-                  elevation: 5,
-                  color: Colors.white,
-                  child: ListTile(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15.0),
-                    ),
-                    contentPadding: const EdgeInsets.all(8.0), // Reduced padding
-                    tileColor: Colors.white,
-                    leading: CircleAvatar(
-                      backgroundColor: Colors.black,
-                      child: Text(
-                        '#$index',
-                        style: const TextStyle(color: Colors.white),
-                      ),
-                    ),
-                    title: Text(
-                      'Cornea Request #$index',
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
-                    ),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const SizedBox(height: 8),
-                        const Text(
-                          'Doctor: Dr. Smith',
-                          style: TextStyle(color: Colors.black54),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: CircleAvatar(
+                          backgroundColor: Colors.black45,
+                          child: Text(
+                            '#$index',
+                            style: const TextStyle(color: Colors.white),
+                          ),
                         ),
-                        const SizedBox(height: 8),
-                        Text(
-                          'Details about cornea request #$index',
-                          style: const TextStyle(color: Colors.black45),
+                      ),
+                      Expanded(
+                        flex: 3,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Cornea Request #$index',
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                            ),
+                            const SizedBox(height: 8),
+                            const Text(
+                              'Doctor: Dr. Smith',
+                              style: TextStyle(color: Colors.black54),
+                            ),
+                            const SizedBox(height: 8),
+                            Text(
+                              'Details about cornea request #$index',
+                              style: const TextStyle(color: Colors.black45),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                    trailing: IconButton(
-                      icon: const Icon(Icons.arrow_forward, color: Colors.black),
-                      onPressed: () {},
-                    ),
+                      ),
+                    ],
                   ),
                 ),
                 childCount: 20,
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
