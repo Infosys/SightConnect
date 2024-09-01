4d5f52f6c6ffa75182b402f20f35f7e3234a836a
Avendra <avendrasingh.gaur@infosys.com>
2024-09-01 12:01:12 +0530

    integrated organ inventory search deligate API

--- lib/apps/eyebank/features/eb_organ_inventory/data/repository/eb_organ_inventory_repo.dart
+++ lib/apps/eyebank/features/eb_organ_inventory/data/repository/eb_organ_inventory_repo.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 import 'package:eye_care_for_all/apps/eyebank/features/eb_organ_inventory/data/model/eb_organ_inventory_analytics_model.dart';
+import 'package:eye_care_for_all/apps/eyebank/features/eb_organ_inventory/data/model/organ_tissue_search_deligate_model.dart';
 import 'package:eye_care_for_all/apps/eyebank/helpers/widgets/eb_error_handler.dart';
 import 'package:eye_care_for_all/services/dio_service.dart';
 import 'package:eye_care_for_all/services/eb_failure.dart';
@@ -10,6 +11,9 @@ abstract class EBOrganInventoryRepository {
   // GET : Organ inventory statistics
   Future<Either<EBFailure, EbOrganInventoryAnalyticsModel>>
       getOrganInventoryStatistics();
+
+  Future<Either<EBFailure, OrganTissueSearchDeligateModel>>
+      getOrganTissueSearchDelegate();
 }
 
 final ebOrganInventoryRepositoryProvider = Provider(
@@ -30,6 +34,22 @@ class EBOrganInventoryRepositoryImpl extends EBOrganInventoryRepository {
       final response = await _dio.get(endPoint);
       if (response.statusCode == 200) {
         final data = EbOrganInventoryAnalyticsModel.fromJson(response.data);
+        return data;
+      } else {
+        throw Exception(response.statusMessage ??
+            'Failed to fetch organ inventory statistics');
+      }
+    });
+  }
+
+  @override
+  Future<Either<EBFailure, OrganTissueSearchDeligateModel>>
+      getOrganTissueSearchDelegate() {
+    return EyeBankErrorHandler.handle(() async {
+      const endPoint = '/services/eyebank/api/encounters/filters/tissues';
+      final response = await _dio.get(endPoint);
+      if (response.statusCode == 200) {
+        final data = OrganTissueSearchDeligateModel.fromJson(response.data);
         return data;
       } else {
         throw Exception(response.statusMessage ??
