b8c1678d13bd0c37bdad05ec58d08e856dff49fb
sarthak <sarthak.paranjpe@infosys.com>
2024-09-24 16:22:55 +0530

    added new initialization changes

---
+++ lib/apps/sightconnect/common/initialization/widgets/splash_shimmer.dart
@@ -0,0 +1,43 @@
+import 'package:eye_care_for_all/apps/sightconnect/common/initialization/pages/initialization_page.dart';
+import 'package:eye_care_for_all/shared/constants/app_size.dart';
+import 'package:eye_care_for_all/shared/pages/pulsar_effect_page.dart';
+import 'package:eye_care_for_all/shared/responsive/responsive.dart';
+import 'package:eye_care_for_all/shared/widgets/app_upgrader.dart';
+import 'package:flutter/material.dart';
+import 'package:matomo_tracker/matomo_tracker.dart';
+
+class SplashShimmer extends StatelessWidget {
+  const SplashShimmer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppUpgrader(
+      child: TraceableWidget(
+        actionName: "InitializationPage",
+        path: InitializationPage.routeName,
+        child: Scaffold(
+          body: Stack(
+            children: <Widget>[
+              Positioned.fill(
+                child: Image.asset(
+                  'assets/logo/splash_bg.png',
+                  fit: BoxFit.fill,
+                ),
+              ),
+              Pulsar(
+                child: Center(
+                  child: Image.asset(
+                    "assets/logo/splash_icon_transparant.png",
+                    width: Responsive.isMobile(context)
+                        ? AppSize.width(context) * 0.3
+                        : AppSize.width(context) * 0.1,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
